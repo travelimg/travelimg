3d6bbfdf6b45fabb94719176a738dc78d188838b
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-05-03 20:05:59 +0200

    ImportServiceImpl: Clarify code and use try-with-resource in readAll

--- src/main/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCPhotoDAO.java
+++ src/main/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCPhotoDAO.java
@@ -8,10 +8,7 @@ import at.ac.tuwien.qse.sepm.entities.validators.ValidationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,8 +16,8 @@ public class JDBCPhotoDAO extends JDBCDAOBase implements PhotoDAO {
 
     private static final Logger logger = LogManager.getLogger();
 
-    private static String insertStatement = "INSERT INTO Photo(photographer_id, path, date) VALUES (1, ?, ?);";
-
+    private static final String insertStatement = "INSERT INTO Photo(photographer_id, path, date) VALUES (1, ?, ?);";
+    private static final String readAllStatement = "SELECT* FROM PHOTO ORDER BY DATE;";
 
     public JDBCPhotoDAO() {
 
@@ -29,27 +26,24 @@ public class JDBCPhotoDAO extends JDBCDAOBase implements PhotoDAO {
     public Photo create(Photo photo) throws DAOException, ValidationException {
         PhotoValidator.validate(photo);
 
-        Connection con = getConnection();
-        try {
-            try(PreparedStatement stmt = con.prepareStatement(insertStatement)) {
-                stmt.setString(1, photo.getPath());
-                stmt.setDate(2, new java.sql.Date(photo.getDate().getTime()));
+        try(PreparedStatement stmt = getConnection().prepareStatement(insertStatement)) {
+            stmt.setString(1, photo.getPath());
+            stmt.setDate(2, new java.sql.Date(photo.getDate().getTime()));
 
-                int affectedRows = stmt.executeUpdate();
-                ResultSet rs = stmt.getGeneratedKeys();
+            int affectedRows = stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
 
-                if(affectedRows != 1 || !rs.next()) {
-                    logger.error("Failed to create photo {}", photo);
-                    throw new DAOException("Failed to create photo");
-                }
-
-                int id = rs.getInt(1);
-                photo.setId(id);
-
-                logger.info("Created photo {}", photo);
-
-                return photo;
+            if(affectedRows != 1 || !rs.next()) {
+                logger.error("Failed to create photo {}", photo);
+                throw new DAOException("Failed to create photo");
             }
+
+            int id = rs.getInt(1);
+            photo.setId(id);
+
+            logger.info("Created photo {}", photo);
+
+            return photo;
         } catch (SQLException e) {
             throw new DAOException(e.getMessage());
         }
@@ -64,16 +58,21 @@ public class JDBCPhotoDAO extends JDBCDAOBase implements PhotoDAO {
     }
 
     public List<Photo> readAll() throws DAOException, ValidationException {
-        Connection con = getConnection();
-
         List<Photo> photos = new ArrayList<Photo>();
-        try {
-            ResultSet rs = con.createStatement().executeQuery("SELECT* FROM PHOTO ORDER BY DATE;");
-            while(rs.next()){
-                photos.add(new Photo(rs.getInt(1),null,rs.getString(3),rs.getDate(4),rs.getInt(5)));
+        try(Statement stmt = getConnection().createStatement()) {
+            ResultSet rs = stmt.executeQuery(readAllStatement);
+
+            while(rs.next()) {
+                photos.add(new Photo(
+                        rs.getInt(1),
+                        null,
+                        rs.getString(3),
+                        rs.getDate(4),
+                        rs.getInt(5)
+                ));
             }
         } catch (SQLException e) {
-           throw new DAOException(e.getMessage());
+            throw new DAOException(e);
         }
 
         return photos;
