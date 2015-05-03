eb10a8bbd6135d230bcb47daaf9a108d6e8fc73f
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-05-03 19:29:49 +0200

    Override all base class constructors in ServiceException

--- src/main/java/at/ac/tuwien/qse/sepm/service/ServiceException.java
+++ src/main/java/at/ac/tuwien/qse/sepm/service/ServiceException.java
@@ -2,10 +2,21 @@ package at.ac.tuwien.qse.sepm.service;
 
 public class ServiceException extends Exception {
     public ServiceException() {
-        super();
     }
 
-    public ServiceException(String message) {
-        super(message);
+    public ServiceException(String s) {
+        super(s);
+    }
+
+    public ServiceException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public ServiceException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public ServiceException(String s, Throwable throwable, boolean b, boolean b1) {
+        super(s, throwable, b, b1);
     }
 }
