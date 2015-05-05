34c0d90389fa63ba0efe26eab663b1c36ea9ea40
David Peherstorfer <david.peherstorfer@live.de>
2015-05-05 22:07:38 +0200

    Migrated importExif to DAO layer

--- src/main/java/at/ac/tuwien/qse/sepm/service/impl/ExifServiceImpl.java
+++ src/main/java/at/ac/tuwien/qse/sepm/service/impl/ExifServiceImpl.java
@@ -37,12 +37,21 @@ public class ExifServiceImpl implements ExifService {
 
     }
 
-    public void changeExif(Exif e) {
-
-//        Metadata metadata = ImageMetadataReader.readMetadata(file);
-//        Directory subIFDDirectory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-//        Directory iFD0Directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
-//        Directory gPSDirectory = metadata.getFirstDirectoryOfType(GpsDirectory.class);
+    public void changeExif(Photo p) throws ServiceException {
+            File file = new File(p.getPath());
+        Metadata metadata = null;
+        try {
+            metadata = ImageMetadataReader.readMetadata(file);
+            Directory subIFDDirectory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+            Directory iFD0Directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+            Directory gPSDirectory = metadata.getFirstDirectoryOfType(GpsDirectory.class);
+        } catch (ImageProcessingException e) {
+            e.printStackTrace();
+            throw new ServiceException(e.getMessage(), e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new ServiceException(e.getMessage(), e);
+        }
 
 //        date = new Timestamp(subIFDDirectory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL).getTime());
 //        exposure = subIFDDirectory.getString(ExifSubIFDDirectory.TAG_EXPOSURE_TIME);
@@ -56,72 +65,11 @@ public class ExifServiceImpl implements ExifService {
 
 
     public Exif importExif(Photo p) throws ServiceException {
-        File file = new File(p.getPath());
-        Timestamp date;
-        String exposure;
-        double aperture;
-        double focalLength;
-        int iso;
-        boolean flash;
-        String cameraModel;
-        Rational[] longitude;
-        String longitudeString;
-        Rational[] latitude;
-        String latitudeString;
-        double altitude = 0;
         try {
-            Metadata metadata = ImageMetadataReader.readMetadata(file);
-            Directory subIFDDirectory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-            Directory iFD0Directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
-            Directory gPSDirectory = metadata.getFirstDirectoryOfType(GpsDirectory.class);
-            try {
-
-                date = new Timestamp(subIFDDirectory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL).getTime());
-                exposure = subIFDDirectory.getString(ExifSubIFDDirectory.TAG_EXPOSURE_TIME);
-                aperture = subIFDDirectory.getDouble(ExifSubIFDDirectory.TAG_APERTURE);
-                focalLength = subIFDDirectory.getDouble(ExifSubIFDDirectory.TAG_FOCAL_LENGTH);
-                iso = subIFDDirectory.getInt(ExifSubIFDDirectory.TAG_ISO_EQUIVALENT);
-                flash = subIFDDirectory.getBoolean(ExifSubIFDDirectory.TAG_FLASH);
-                cameraModel = iFD0Directory.getString(ExifIFD0Directory.TAG_MAKE) + " " + iFD0Directory.getString(ExifIFD0Directory.TAG_MODEL);
-
-                longitude = gPSDirectory.getRationalArray(GpsDirectory.TAG_LONGITUDE);
-                latitude = gPSDirectory.getRationalArray(GpsDirectory.TAG_LATITUDE);
-                altitude = gPSDirectory.getDouble(GpsDirectory.TAG_ALTITUDE);
-
-                if(longitude == null || latitude == null) {
-                    throw new MetadataException("No longitude/latitude data set");
-                }
-                double longitudeHours = longitude[0].doubleValue();
-                double longitudeMinutes = longitude[1].doubleValue();
-                double longitudeSeconds = (longitude[1].doubleValue() - Math.floor(longitude[1].doubleValue())) * 60;
-                longitudeString = longitudeHours + " " + longitudeMinutes + " " + longitudeSeconds + " " + gPSDirectory.getString(GpsDirectory.TAG_LONGITUDE_REF);
-
-                double latitudeHours = latitude[0].doubleValue();
-                double latitudeMinutes = latitude[1].doubleValue();
-                double latitudeSeconds = (latitude[1].doubleValue() - Math.floor(latitude[1].doubleValue())) * 60;
-                latitudeString = latitudeHours + " " + latitudeMinutes + " " + latitudeSeconds + " " + gPSDirectory.getString(GpsDirectory.TAG_LATITUDE_REF);
-
-
-
-
-                Exif exif = new Exif(p.getId(), date, exposure, aperture, focalLength, iso, flash, cameraModel, longitudeString, latitudeString, altitude);
-                exifDAO.create(exif);
-                p.setExif(exif);
-                return exif;
-            } catch (MetadataException e) {
-                e.printStackTrace();
-                throw new ServiceException(e.getMessage(), e);
-            } catch (DAOException e) {
-                e.printStackTrace();
-                throw new ServiceException(e.getMessage(), e);
-            }
-        } catch (ImageProcessingException e) {
-            e.printStackTrace();
-            throw new ServiceException(e.getMessage(), e);
-        } catch (IOException e) {
+            return exifDAO.importExif(p);
+        } catch (DAOException e) {
             e.printStackTrace();
             throw new ServiceException(e.getMessage(), e);
         }
     }
-
 }
