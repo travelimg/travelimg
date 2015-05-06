ebb6e5ce2b6d34a4dedb390d89417f021ccffc84
Enri Miho <enrimiho@gmail.com>
2015-05-07 01:54:17 +0200

    PhotographerDAO: commented the method read in javadoc.

--- src/main/java/at/ac/tuwien/qse/sepm/dao/PhotographerDAO.java
+++ src/main/java/at/ac/tuwien/qse/sepm/dao/PhotographerDAO.java
@@ -12,12 +12,19 @@ public interface PhotographerDAO {
      *
      * @param p Photographer to create
      * @return The created photographer
+     * @throws ValidationException If the photographer is invalid
      * @throws DAOException If the photographer cannot be copied or the data store fails to create a record.
      */
     public Photographer create(Photographer p) throws DAOException, ValidationException;
 
-
+    /**
+     * Reads a single photographer by the id
+     * @param p the photographer object containing the id
+     * @return the read photographer
+     * @throws DAOException If the data store fails to retrieve the record or if the photographer doesn't exist.
+     */
     public Photographer read(Photographer p) throws DAOException;
+
 
     public List<Photographer> readAll() throws DAOException;
 }
