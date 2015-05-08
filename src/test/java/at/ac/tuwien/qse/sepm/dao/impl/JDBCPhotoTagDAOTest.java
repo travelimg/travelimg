3754d5a10a854ba0cb622f028e144efe63e3f69b
Christoph Wasylewski <e1226587@student.tuwien.ac.at>
2015-05-08 17:56:51 +0200

    editing deletePhotoTagTest

--- src/test/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCPhotoTagDAOTest.java
+++ src/test/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCPhotoTagDAOTest.java
@@ -3,6 +3,7 @@ package at.ac.tuwien.qse.sepm.dao.impl;
 import at.ac.tuwien.qse.sepm.dao.AbstractJDBCPhotoTagDAOTest;
 import at.ac.tuwien.qse.sepm.dao.PhotoDAO;
 import at.ac.tuwien.qse.sepm.dao.PhotoTagDAO;
+import at.ac.tuwien.qse.sepm.dao.impl.JDBCPhotoTagDAO;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
@@ -14,12 +15,9 @@ public class JDBCPhotoTagDAOTest extends AbstractJDBCPhotoTagDAOTest {
 
     @Before
     public void setUp() throws Exception {
-        //erstellt H2 Verbindung
-        //ggf. Datenbank erstellen (sofern In-Memory und nicht Server-Mode)
-        //Testdaten einspielen
-        //erstelle H2JockeyDAO
+
         PhotoTagDAO pt = new JDBCPhotoTagDAO();
-        //setzte JockeyDAO
+
         setPhotoTagDAO(pt);
         DBConnection.getConnection().setAutoCommit(false);
     }
