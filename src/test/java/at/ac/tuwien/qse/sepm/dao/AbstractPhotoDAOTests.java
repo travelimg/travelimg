e6c2a1824098c7b25218b216fe5d5d124d367692
Christoph Wasylewski <e1226587@student.tuwien.ac.at>
2015-05-08 10:57:21 +0200

    solve errors

--- src/test/java/at/ac/tuwien/qse/sepm/dao/AbstractPhotoDAOTests.java
+++ src/test/java/at/ac/tuwien/qse/sepm/dao/AbstractPhotoDAOTests.java
@@ -4,13 +4,13 @@ import at.ac.tuwien.qse.sepm.entities.Photo;
 import at.ac.tuwien.qse.sepm.entities.validators.ValidationException;
 import org.junit.Test;
 
-<<<<<<< HEAD
+
 import static org.mockito.Mockito.mock;
-=======
+
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
->>>>>>> develop
+
 
 public abstract class AbstractPhotoDAOTests {
 
@@ -26,14 +26,14 @@ public abstract class AbstractPhotoDAOTests {
     }
 
     @Test
-<<<<<<< HEAD
+
     public void testDelete() throws DAOException, ValidationException {
         Photo mockPhoto = mock(Photo.class);
         photoDAO.create(mockPhoto);
-=======
+    }
     public void testReadPhotosByYearAndMonthShouldReturnPhotos() throws DAOException {
         List<Photo> photos = photoDAO.readPhotosByYearAndMonth(2015,3);
         assertTrue(photos.size() > 0);
->>>>>>> develop
+
     }
 }
