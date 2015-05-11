07f6c8dbdf847335620f590ec38a376a1467cd77
Christoph Wasylewski <e1226587@student.tuwien.ac.at>
2015-05-11 09:48:46 +0200

    implement into gui

--- src/main/java/at/ac/tuwien/qse/sepm/gui/Inspector.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/Inspector.java
@@ -1,11 +1,17 @@
 package at.ac.tuwien.qse.sepm.gui;
 
 import at.ac.tuwien.qse.sepm.entities.Photo;
+import at.ac.tuwien.qse.sepm.service.PhotoService;
+import at.ac.tuwien.qse.sepm.service.ServiceException;
+import at.ac.tuwien.qse.sepm.service.impl.PhotoServiceImpl;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Controller for the inspector view which is used for modifying meta-data of a photo.
@@ -20,9 +26,10 @@ public class Inspector {
     @FXML private Label proofOfConceptLabel;
 
     private Photo photo = null;
+    private PhotoService photoservice;
 
     public Inspector() {
-
+        photoservice = new PhotoServiceImpl();
     }
 
     /**
@@ -46,7 +53,16 @@ public class Inspector {
     }
 
     private void handleDelete(Event event) {
-        // TODO
+        if(photo!=null){
+            System.out.println(photo.toString());
+            List<Photo> photolist = new ArrayList<Photo>();
+            photolist.add(photo);
+            try {
+                photoservice.deletePhotos(photolist);
+            } catch (ServiceException e) {
+                System.out.println("Fehler beim löschen");
+            }
+        }
     }
 
     private void handleCancel(Event event) {
