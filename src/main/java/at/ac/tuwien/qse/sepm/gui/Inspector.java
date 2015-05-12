f69a985ba5cfa4528a8721c5516fcc7d3952ed5a
David Peherstorfer <david.peherstorfer@live.de>
2015-05-12 22:10:49 +0200

    Finished Exif-data table:
    - Every data is now shown.

--- src/main/java/at/ac/tuwien/qse/sepm/gui/Inspector.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/Inspector.java
@@ -1,6 +1,5 @@
 package at.ac.tuwien.qse.sepm.gui;
 
-
 import at.ac.tuwien.qse.sepm.entities.Exif;
 import at.ac.tuwien.qse.sepm.entities.Photo;
 import com.lynden.gmapsfx.GoogleMapView;
@@ -9,21 +8,20 @@ import at.ac.tuwien.qse.sepm.entities.Photo;
 import at.ac.tuwien.qse.sepm.service.PhotoService;
 import at.ac.tuwien.qse.sepm.service.ServiceException;
 
-import javafx.beans.property.ReadOnlyObjectWrapper;
-import javafx.beans.property.ReadOnlyStringWrapper;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.util.Callback;
 import javafx.util.Pair;
 import org.controlsfx.tools.Platform;
 
@@ -32,7 +30,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
-
 
 /**
  * Controller for the inspector view which is used for modifying meta-data of a photo.
@@ -44,24 +41,15 @@ public class Inspector {
     @FXML private Button cancelButton;
     @FXML private Button confirmButton;
 
-    @FXML
-    private TableColumn<Pair<String, Object>, Object> exifValue;
+    @FXML private TableColumn<String, String> exifValue;
 
-    @FXML
-    private TableColumn<Pair<String, Object>, Object> exifName;
+    @FXML private TableColumn<String, String> exifName;
 
-    @FXML
-    private TableView<Pair<String, Object>> exifTable;
-
-
-
-//   private Photo photo = null;
+    @FXML private TableView<Pair<String, String>> exifTable;
 
     @FXML private VBox contentBox2;
 
-   // @FXML private Label proofOfConceptLabel;
-
-   private GoogleMapsScene mapsScene;
+    private GoogleMapsScene mapsScene;
 
     @FXML private Label proofOfConceptLabel;
 
@@ -70,14 +58,13 @@ public class Inspector {
     private Photo photo = null;
     @Autowired private PhotoService photoservice;
 
-
     public Inspector() {
 
     }
 
     /**
      * Set the active photo.
-     *
+     * <p>
      * The photos metadate will be displayed in the inspector widget.
      *
      * @param photo The active photo for which to show further information
@@ -88,30 +75,21 @@ public class Inspector {
         proofOfConceptLabel.setText("Selected photo is: " + photo.getPath());
 
         Exif exif = photo.getExif();
-        ObservableList<Pair<String, Object>> exifData = FXCollections.observableArrayList(
-                new Pair<String, Object>("Aufnahmedatum", exif.getDate().toString()),
-                new Pair<String, Object>("Kamerahersteller", exif.getMake()),
-                new Pair<String, Object>("Kameramodell", exif.getModel())
-        );
+        ObservableList<Pair<String, String>> exifData = FXCollections.observableArrayList(
+                new Pair<String, String>("Aufnahmedatum", exif.getDate().toString()),
+                new Pair<String, String>("Kamerahersteller", exif.getMake()),
+                new Pair<String, String>("Kameramodell", exif.getModel()),
+                new Pair<String, String>("Belichtungszeit", exif.getExposure() + " Sek."),
+                new Pair<String, String>("Blende", "f/" + exif.getAperture()),
+                new Pair<String, String>("Brennweite", "" + exif.getFocalLength()),
+                new Pair<String, String>("ISO", "" + exif.getIso()),
+                new Pair<String, String>("Blitz", exif.isFlash()? "wurde ausgelöst" : "wurde nicht ausgelöst"),
+                new Pair<String, String>("Höhe", "" + exif.getAltitude()));
 
         exifTable.setEditable(true);
-        exifName.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Pair<String, Object>, Object>, ObservableValue<Object>>() {
-            @Override
-            public ObservableValue<Object> call(TableColumn.CellDataFeatures<Pair<String, Object>, Object> param) {
-                return new ReadOnlyObjectWrapper(param.getValue().getKey());
-            }
-        });
-
-        exifValue.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Pair<String, Object>, Object>, ObservableValue<Object>>() {
-            @Override
-            public ObservableValue<Object> call(TableColumn.CellDataFeatures<Pair<String, Object>, Object> param) {
-                return new ReadOnlyObjectWrapper(param.getValue().getKey());
-            }
-        });
-
-        exifValue.setCellFactory((param) -> new ExifValueCell());
+        exifName.setCellValueFactory(new PropertyValueFactory<String, String>("Key"));
+        exifValue.setCellValueFactory(new PropertyValueFactory<String, String>("Value"));
         exifTable.setItems(exifData);
-
         //this.mapsScene = new GoogleMapsScene(photo.getExif());
         //contentBox2.getChildren().clear();
 
@@ -119,27 +97,23 @@ public class Inspector {
 
         mapsScene.addMarker(photo);
 
-
     }
 
-    @FXML
-    private void initialize() {
+    @FXML private void initialize() {
         deleteButton.setOnAction(this::handleDelete);
         cancelButton.setOnAction(this::handleCancel);
         confirmButton.setOnAction(this::handleConfirm);
-        this.mapsScene= new GoogleMapsScene();
+        this.mapsScene = new GoogleMapsScene();
         contentBox2.getChildren().add(mapsScene.getMapView());
-
-
 
     }
 
     private void handleDelete(Event event) {
-        if(photo!=null){
+        if (photo != null) {
 
             List<Photo> photolist = new ArrayList<Photo>();
             photolist.add(photo);
-           organizer.reloadPhotos();
+            organizer.reloadPhotos();
             try {
                 photoservice.deletePhotos(photolist);
             } catch (ServiceException e) {
@@ -154,32 +128,5 @@ public class Inspector {
 
     private void handleConfirm(Event event) {
         // TODO
-    }
-
-    class ExifValueCell extends TableCell<Pair<String, Object>, Object> {
-        @Override
-        protected void updateItem(Object item, boolean empty) {
-            super.updateItem(item, empty);
-
-            if (item != null) {
-                if (item instanceof String) {
-                    TextField f = new TextField((String) item); // z.b. ein textfield zum editieren. Hier kann auch ein signalhandler auf aenderungen horchen
-                    setGraphic(f);
-                } else if (item instanceof Integer) {
-                    setText(Integer.toString((Integer) item));
-                    setGraphic(null);
-                } else if (item instanceof Boolean) {
-                    CheckBox checkBox = new CheckBox();
-                    checkBox.setSelected((boolean) item);
-                    setGraphic(checkBox);
-                } else {
-                    setText("N/A");
-                    setGraphic(null);
-                }
-            } else {
-                setText(null);
-                setGraphic(null);
-            }
-        }
     }
 }
