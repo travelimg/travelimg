b5dbf606328c6c3bc21d20735f76020a0c84db14
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2015-05-15 16:03:05 +0200

    Clean up Inspector code.

--- src/main/java/at/ac/tuwien/qse/sepm/gui/Inspector.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/Inspector.java
@@ -36,89 +36,65 @@ import java.util.List;
  */
 public class Inspector {
 
-    @FXML private BorderPane root;
     @FXML private Button deleteButton;
     @FXML private Button cancelButton;
     @FXML private Button confirmButton;
-
+    @FXML private VBox mapContainer;
     @FXML private TableColumn<String, String> exifValue;
-
     @FXML private TableColumn<String, String> exifName;
-
     @FXML private TableView<Pair<String, String>> exifTable;
 
-    @FXML private VBox contentBox2;
-
-    private GoogleMapsScene mapsScene;
-
-    @FXML private Label proofOfConceptLabel;
+    private final GoogleMapsScene mapsScene = new GoogleMapsScene();
+    private Photo photo = null;
 
     @Autowired private Organizer organizer;
-
-    private Photo photo = null;
     @Autowired private PhotoService photoservice;
-
-    public Inspector() {
-
-    }
 
     /**
      * Set the active photo.
-     * <p>
-     * The photos metadate will be displayed in the inspector widget.
+     *
+     * The photos metadata will be displayed in the inspector widget.
      *
      * @param photo The active photo for which to show further information
      */
     public void setActivePhoto(Photo photo) {
         this.photo = photo;
 
-        proofOfConceptLabel.setText("Selected photo is: " + photo.getPath());
-
         Exif exif = photo.getExif();
         ObservableList<Pair<String, String>> exifData = FXCollections.observableArrayList(
-                new Pair<String, String>("Aufnahmedatum", exif.getDate().toString()),
-                new Pair<String, String>("Kamerahersteller", exif.getMake()),
-                new Pair<String, String>("Kameramodell", exif.getModel()),
-                new Pair<String, String>("Belichtungszeit", exif.getExposure() + " Sek."),
-                new Pair<String, String>("Blende", "f/" + exif.getAperture()),
-                new Pair<String, String>("Brennweite", "" + exif.getFocalLength()),
-                new Pair<String, String>("ISO", "" + exif.getIso()),
-                new Pair<String, String>("Blitz", exif.isFlash()? "wurde ausgelöst" : "wurde nicht ausgelöst"),
-                new Pair<String, String>("Höhe", "" + exif.getAltitude()));
-
-        exifTable.setEditable(true);
-        exifName.setCellValueFactory(new PropertyValueFactory<String, String>("Key"));
-        exifValue.setCellValueFactory(new PropertyValueFactory<String, String>("Value"));
+                new Pair<>("Aufnahmedatum", exif.getDate().toString()),
+                new Pair<>("Kamerahersteller", exif.getMake()),
+                new Pair<>("Kameramodell", exif.getModel()),
+                new Pair<>("Belichtungszeit", exif.getExposure() + " Sek."),
+                new Pair<>("Blende", "f/" + exif.getAperture()),
+                new Pair<>("Brennweite", "" + exif.getFocalLength()),
+                new Pair<>("ISO", "" + exif.getIso()),
+                new Pair<>("Blitz", exif.isFlash()? "wurde ausgelöst" : "wurde nicht ausgelöst"),
+                new Pair<>("Höhe", "" + exif.getAltitude()));
+        exifName.setCellValueFactory(new PropertyValueFactory<>("Key"));
+        exifValue.setCellValueFactory(new PropertyValueFactory<>("Value"));
         exifTable.setItems(exifData);
-        //this.mapsScene = new GoogleMapsScene(photo.getExif());
-        //contentBox2.getChildren().clear();
 
-        //contentBox2.getChildren().add(mapsScene.getMapView());
-        mapsScene.setMaxSize(200,200);
         mapsScene.addMarker(photo);
-
     }
 
     @FXML private void initialize() {
         deleteButton.setOnAction(this::handleDelete);
         cancelButton.setOnAction(this::handleCancel);
         confirmButton.setOnAction(this::handleConfirm);
-        this.mapsScene = new GoogleMapsScene();
-        contentBox2.getChildren().add(mapsScene.getMapView());
-
+        mapContainer.getChildren().add(mapsScene.getMapView());
     }
 
     private void handleDelete(Event event) {
-        if (photo != null) {
+        if (photo == null) return;
 
-            List<Photo> photolist = new ArrayList<Photo>();
-            photolist.add(photo);
-            organizer.reloadPhotos();
-            try {
-                photoservice.deletePhotos(photolist);
-            } catch (ServiceException e) {
-                System.out.println(e);
-            }
+        List<Photo> photos = new ArrayList<>();
+        photos.add(photo);
+        organizer.reloadPhotos();
+        try {
+            photoservice.deletePhotos(photos);
+        } catch (ServiceException e) {
+            System.out.println(e);
         }
     }
 
