afffb5489d124ac93a16b6e3f14ceca1a1ca8d8a
Cryt1c <david.peherstorfer@live.de>
2015-05-17 14:41:21 +0200

    Add ClusteringService

--- src/main/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCDAOBase.java
+++ src/main/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCDAOBase.java
@@ -1,14 +1,11 @@
 package at.ac.tuwien.qse.sepm.dao.impl;
 
-
-import at.ac.tuwien.qse.sepm.dao.DAOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 
 public class JDBCDAOBase {
 
@@ -20,18 +17,4 @@ public class JDBCDAOBase {
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
-
-    private Connection con = null;
-
-    /**
-     * @deprecated
-     */
-    protected Connection getConnection() throws DAOException {
-        logger.warn("Deprecated use of getConnection(). Use jdbcTemplate.");
-        if(con == null)
-            con = DBConnection.getConnection();
-
-        return con;
-    }
-
 }
---
+++ src/main/java/at/ac/tuwien/qse/sepm/service/ClusterService.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.qse.sepm.service;
+
+import at.ac.tuwien.qse.sepm.entities.Photo;
+
+import java.util.List;
+
+/**
+ * Created by David on 15.05.2015.
+ */
+public interface ClusterService {
+    void cluster (List<Photo> photos);
+}
