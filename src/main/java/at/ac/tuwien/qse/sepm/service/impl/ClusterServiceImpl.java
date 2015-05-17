7de94398178b9814d0fa5baeb43912bf7abe188a
David Peherstorfer <david.peherstorfer@live.de>
2015-05-17 15:35:33 +0200

    Add ClusteringServiceTest

---
+++ src/main/java/at/ac/tuwien/qse/sepm/service/impl/ClusterServiceImpl.java
@@ -0,0 +1,43 @@
+package at.ac.tuwien.qse.sepm.service.impl;
+
+import at.ac.tuwien.qse.sepm.entities.Photo;
+import at.ac.tuwien.qse.sepm.service.ClusterService;
+import org.apache.commons.math3.ml.clustering.Cluster;
+import org.apache.commons.math3.ml.clustering.DBSCANClusterer;
+import org.apache.commons.math3.ml.clustering.DoublePoint;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoField;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by David on 15.05.2015.
+ */
+public class ClusterServiceImpl implements ClusterService {
+
+    private static final org.apache.logging.log4j.Logger logger = LogManager
+            .getLogger(ClusterServiceImpl.class);
+    private static final DBSCANClusterer dbscan = new DBSCANClusterer(.05, 3);
+
+    @Override public void cluster(List<Photo> photos) {
+        logger.debug("photoList-size: " + photos.size());
+        List<DoublePoint> pointList = new ArrayList<DoublePoint>();
+        ZoneId zoneId = ZoneId.systemDefault();
+        for(Photo photo: photos) {
+            double[] date ={photo.getExif().getDate().atStartOfDay(zoneId).toEpochSecond()};
+            pointList.add(new DoublePoint(date));
+        }
+        logger.debug("pointList-size: " + pointList.size());
+
+        List<Cluster<DoublePoint>> cluster = dbscan.cluster(pointList);
+
+        for(Cluster<DoublePoint> c : cluster){
+            logger.debug(c.getPoints().get(0));
+        }
+
+    }
+}
---
+++ src/test/java/at/ac/tuwien/qse/sepm/dao/impl/ClusterServiceTest.java
@@ -0,0 +1,46 @@
+package at.ac.tuwien.qse.sepm.dao.impl;
+
+import at.ac.tuwien.qse.sepm.dao.DAOException;
+import at.ac.tuwien.qse.sepm.dao.PhotoDAO;
+import at.ac.tuwien.qse.sepm.dao.UsingTable;
+import at.ac.tuwien.qse.sepm.dao.WithData;
+import at.ac.tuwien.qse.sepm.entities.validators.ValidationException;
+import at.ac.tuwien.qse.sepm.service.impl.ClusterServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by David on 17.05.2015.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:test-config.xml")
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@UsingTable("Photo")
+public class ClusterServiceTest {
+
+    @Autowired
+    ClusterServiceImpl clusterService;
+
+    @Autowired
+    PhotoDAO photoDAO;
+
+    @WithData
+    @Test
+    public void testClusteringService() {
+        try {
+            clusterService.cluster(photoDAO.readAll());
+        } catch (DAOException e) {
+            e.printStackTrace();
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+}
