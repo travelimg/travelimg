ed254f9bcad9e262539ae85e1a7ffaa448638d9f
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-05-17 20:04:37 +0200

    Add Dropbox service interface

---
+++ src/main/java/at/ac/tuwien/qse/sepm/service/DropboxService.java
@@ -0,0 +1,32 @@
+package at.ac.tuwien.qse.sepm.service;
+
+import at.ac.tuwien.qse.sepm.entities.Photo;
+import at.ac.tuwien.qse.sepm.util.Cancelable;
+import at.ac.tuwien.qse.sepm.util.ErrorHandler;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public interface DropboxService extends Service {
+
+    /**
+     * Fetch the dropbox base directory.
+     *
+     * @return The base directory of the users dropbox.
+     */
+    String getDropboxFolder();
+
+    /**
+     * Upload a given set of photos to the user dropbox.
+     *
+     * The exact folder is specified by the user and the upload happens in the background.
+     *
+     * @param photos The photos to upload.
+     * @param path The path relative to the dropbox base where to the photos are uploaded.
+     * @param callback Callback which is called after a photo is finished uploading.
+     * @param errorHandler Handler for occuring exceptions.
+     *
+     * @return A cancelable task for aborting the upload if desired.
+     */
+    Cancelable uploadPhotos(List<Photo> photos, String path, Consumer<Photo> callback, ErrorHandler<ServiceException> errorHandler);
+}
