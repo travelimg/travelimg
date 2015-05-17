e2352e20aa148dffb0331d6b9d247395b6e75a2d
David Peherstorfer <david.peherstorfer@live.de>
2015-05-17 16:10:14 +0200

    Start clustering test

--- src/main/java/at/ac/tuwien/qse/sepm/service/impl/ClusterServiceImpl.java
+++ src/main/java/at/ac/tuwien/qse/sepm/service/impl/ClusterServiceImpl.java
@@ -21,14 +21,14 @@ public class ClusterServiceImpl implements ClusterService {
 
     private static final org.apache.logging.log4j.Logger logger = LogManager
             .getLogger(ClusterServiceImpl.class);
-    private static final DBSCANClusterer dbscan = new DBSCANClusterer(.05, 3);
+    private static final DBSCANClusterer dbscan = new DBSCANClusterer(.05, 1);
 
     @Override public void cluster(List<Photo> photos) {
         logger.debug("photoList-size: " + photos.size());
         List<DoublePoint> pointList = new ArrayList<DoublePoint>();
         ZoneId zoneId = ZoneId.systemDefault();
         for(Photo photo: photos) {
-            double[] date ={photo.getExif().getDate().atStartOfDay(zoneId).toEpochSecond()};
+            double[] date ={photo.getDate().atStartOfDay(zoneId).toEpochSecond()};
             pointList.add(new DoublePoint(date));
         }
         logger.debug("pointList-size: " + pointList.size());
