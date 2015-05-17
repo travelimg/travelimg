dffe7db5cb982ba77ec364b0c6a2e17b2208beb7
Enri Miho <enrimiho@gmail.com>
2015-05-18 01:26:47 +0200

    Validate Tag before creating it and test it.

--- src/test/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCTagDAOTest.java
+++ src/test/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCTagDAOTest.java
@@ -1,7 +1,7 @@
 package at.ac.tuwien.qse.sepm.dao.impl;
 
 import at.ac.tuwien.qse.sepm.dao.*;
-import at.ac.tuwien.qse.sepm.entities.Tag;
+import at.ac.tuwien.qse.sepm.entities.validators.ValidationException;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,18 +16,17 @@ public class JDBCTagDAOTest extends AbstractJDBCDAOTest {
     @Test
     public void testWithEmptyDB() throws DAOException {
         assertEquals(0, countRows());
-        assertEquals(0, countRowsWhere("name = 'Test'"));
-        tagDAO.create(new Tag(1, "Test"));
-        assertEquals(1, countRows());
-        assertEquals(1, countRowsWhere("name = 'Test'"));
-        assertEquals(0, countRowsWhere("name = 'Unknown'"));
     }
 
     @Test
     @WithData
     public void testWithData() throws DAOException {
         assertEquals(2, countRows());
-        tagDAO.create(new Tag(1, "Test"));
-        assertEquals(3, countRows());
+    }
+
+    @Test(expected = ValidationException.class)
+    @WithData
+    public void createWithNullShouldThrow() throws ValidationException, DAOException {
+        tagDAO.create(null);
     }
 }
