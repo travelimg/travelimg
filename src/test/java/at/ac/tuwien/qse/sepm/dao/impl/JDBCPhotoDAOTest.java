f334e29ef182ceb871ff7521ed8529679d8c77da
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-05-17 11:02:12 +0200

    Start implementing photoDAO tests

--- src/test/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCPhotoDAOTest.java
+++ src/test/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCPhotoDAOTest.java
@@ -1,10 +1,22 @@
 package at.ac.tuwien.qse.sepm.dao.impl;
 
 import at.ac.tuwien.qse.sepm.dao.*;
+import at.ac.tuwien.qse.sepm.entities.Photo;
+import at.ac.tuwien.qse.sepm.entities.Photographer;
+import at.ac.tuwien.qse.sepm.entities.validators.ValidationException;
+import javafx.util.Pair;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
@@ -15,18 +27,72 @@ public class JDBCPhotoDAOTest extends AbstractJDBCDAOTest {
     @Autowired
     PhotoDAO photoDAO;
 
+    private static final Photographer defaultPhotographer = new Photographer(1, "Test Photographer");
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd", Locale.ENGLISH);
+
     private final String dataDir = Paths.get(System.getProperty("java.io.tmpdir"), "travelimg").toString();
+    private final String sourceDir = Paths.get(getClass().getClassLoader().getResource("db/testimages").getPath()).toString();
+
+    private Pair<String, String> getPhotoSourceDest(Integer id) {
+        if (id < 6 || id > 8) return null;
+
+        String date = dateFormatter.format(LocalDate.now());
+
+        String source = Paths.get(sourceDir, id.toString() + ".jpg").toString();
+        String dest = Paths.get(dataDir, date + "/" + id.toString() + ".jpg").toString();
+
+        return new Pair<>(source, dest);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        FileUtils.deleteDirectory(new File(dataDir));
+        FileUtils.copyDirectory(new File(Paths.get(sourceDir, "prepared").toString()), new File(dataDir));
+    }
 
     @Test
-    public void testWithEmptyDB() throws DAOException {
+    public void testEmpty() throws DAOException {
         assertEquals(0, countRows());
-        assertTrue(false);
     }
 
     @Test
     @WithData
     public void testWithData() throws DAOException {
-        // fails because test_data_insert.sql is incomplete
-        assertEquals(1, countRows());
+        assertEquals(5, countRows());
     }
+
+    @Test
+    @WithData
+    public void testCreateResultHasCorrectAttributes() throws DAOException, ValidationException {
+        Pair<String, String> paths = getPhotoSourceDest(6);
+        Photo photo = new Photo(-1, defaultPhotographer, paths.getKey(), 0, LocalDate.now(), -30, 30);
+        Photo expected = new Photo(-1, defaultPhotographer, paths.getValue(), 0, LocalDate.now(), -30, 30);
+
+        Photo value = photoDAO.create(photo);
+
+        expected.setId(value.getId());
+        assertEquals(expected, value);
+    }
+
+    @Test
+    @WithData
+    public void testCreateImageFileIsCopied() throws DAOException, ValidationException {
+        Pair<String, String> paths = getPhotoSourceDest(6);
+        Photo photo = new Photo(-1, defaultPhotographer, paths.getKey(), 0, LocalDate.now(), -30, 30);
+
+        Photo value = photoDAO.create(photo);
+
+        assertEquals(paths.getValue(), value.getPath());
+        assertTrue(Files.exists(Paths.get(paths.getValue())));
+    }
+
+    @Test(expected = DAOException.class)
+    public void testCreateWithUnknownPhotographerThrows() throws DAOException, ValidationException {
+        Pair<String, String> paths = getPhotoSourceDest(6);
+        Photo photo = new Photo(-1, new Photographer(-42, "Doesnotexist"), paths.getKey(), 0, LocalDate.now(), -30, 30);
+
+        photoDAO.create(photo);
+    }
+
+
 }
