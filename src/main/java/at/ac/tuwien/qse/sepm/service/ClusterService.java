566a255472c06b5e823c5036b628beae9993b992
David Peherstorfer <david.peherstorfer@live.de>
2015-05-18 19:13:54 +0200

    Add entity Reise

--- src/main/java/at/ac/tuwien/qse/sepm/service/ClusterService.java
+++ src/main/java/at/ac/tuwien/qse/sepm/service/ClusterService.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.qse.sepm.service;
 
 import at.ac.tuwien.qse.sepm.entities.Photo;
+import at.ac.tuwien.qse.sepm.entities.Reise;
 
 import java.util.List;
 
@@ -8,5 +9,5 @@ import java.util.List;
  * Created by David on 15.05.2015.
  */
 public interface ClusterService {
-    void cluster (List<Photo> photos);
+    List<Reise> cluster (List<Photo> photos);
 }
--- src/main/java/at/ac/tuwien/qse/sepm/service/impl/ClusterServiceImpl.java
+++ src/main/java/at/ac/tuwien/qse/sepm/service/impl/ClusterServiceImpl.java
@@ -1,19 +1,13 @@
 package at.ac.tuwien.qse.sepm.service.impl;
 
 import at.ac.tuwien.qse.sepm.entities.Photo;
+import at.ac.tuwien.qse.sepm.entities.Reise;
 import at.ac.tuwien.qse.sepm.service.ClusterService;
-import at.ac.tuwien.qse.sepm.service.wrapper.LocationWrapper;
 import at.ac.tuwien.qse.sepm.service.wrapper.TimeWrapper;
-import org.apache.commons.math3.ml.clustering.CentroidCluster;
 import org.apache.commons.math3.ml.clustering.Cluster;
 import org.apache.commons.math3.ml.clustering.DBSCANClusterer;
-import org.apache.commons.math3.ml.clustering.DoublePoint;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Logger;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,10 +20,11 @@ public class ClusterServiceImpl implements ClusterService {
             .getLogger(ClusterServiceImpl.class);
     private static final DBSCANClusterer dbscan = new DBSCANClusterer(604800, 0);
 
-    @Override public void cluster(List<Photo> photos) {
+    @Override public List<Reise> cluster(List<Photo> photos) {
         logger.debug("photoList-size: " + photos.size());
 
         List<TimeWrapper> pointList = new ArrayList<TimeWrapper>();
+        List<Reise> reiseList = new ArrayList<Reise>();
 
         for(Photo photo: photos) {
             pointList.add(new TimeWrapper(photo));
@@ -40,11 +35,20 @@ public class ClusterServiceImpl implements ClusterService {
         List<Cluster<TimeWrapper>> clusterResults = dbscan.cluster(pointList);
 
         for (int i=0; i<clusterResults.size(); i++) {
-            System.out.println("Cluster " + i);
-            for (TimeWrapper timeWrapper : clusterResults.get(i).getPoints())
-                System.out.println(timeWrapper.getPhoto().getDate());
-            System.out.println();
+            double latitude, longitude;
+            int count;
+            latitude = longitude = count = 0;
+//            System.out.println("Cluster " + i);
+            for (TimeWrapper timeWrapper : clusterResults.get(i).getPoints()) {
+                latitude += timeWrapper.getPhoto().getLatitude();
+                longitude += timeWrapper.getPhoto().getLongitude();
+            }
+            reiseList.add(new Reise(latitude/count, longitude/count, ""));
+//                System.out.println(timeWrapper.getPhoto().getDate());
+//            System.out.println();
         }
 
+
+        return reiseList;
     }
 }
