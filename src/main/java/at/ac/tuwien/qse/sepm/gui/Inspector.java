4bf5d45140fc477c39ed2943fb68e31d1d93187f
maristos <marian.sto@aon.at>
2015-05-18 12:50:20 +0200

    Add tagList and tagListChangeListener as fields for the tag-list. Add Initialization at application start and refresh when a new photo is selected.
    - import controlsFX (add dependency in pom.xml)
    - add fields: CheckListView<Tag> tagList, ListChangeListener tagListChangeListener
    - add method: 'initializeTagList' to initialize tagList and tagListChangeListener
    - add method: 'showCurrentlySetTags' to switch tag selection when a new photo is selected

--- src/main/java/at/ac/tuwien/qse/sepm/gui/Inspector.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/Inspector.java
@@ -2,22 +2,28 @@ package at.ac.tuwien.qse.sepm.gui;
 
 import at.ac.tuwien.qse.sepm.entities.Exif;
 import at.ac.tuwien.qse.sepm.entities.Photo;
+import at.ac.tuwien.qse.sepm.entities.Tag;
 import at.ac.tuwien.qse.sepm.service.ExifService;
 import at.ac.tuwien.qse.sepm.service.PhotoService;
 import at.ac.tuwien.qse.sepm.service.ServiceException;
 import javafx.beans.binding.Bindings;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxListCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 import javafx.util.Pair;
+import javafx.util.StringConverter;
+import org.controlsfx.control.CheckListView;
+import org.controlsfx.control.PropertySheet;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
@@ -35,6 +41,8 @@ public class Inspector {
     @FXML private Button cancelButton;
     @FXML private Button confirmButton;
     @FXML private VBox mapContainer;
+    @FXML private CheckListView<Tag> tagList;
+    private ListChangeListener<Tag> tagListChangeListener;
     @FXML private TableColumn<String, String> exifName;
     @FXML private TableColumn<String, String> exifValue;
     @FXML private TableView<Pair<String, String>> exifTable;
@@ -45,9 +53,10 @@ public class Inspector {
     @Autowired private Organizer organizer;
     @Autowired private PhotoService photoservice;
     @Autowired private ExifService exifService;
+
     /**
      * Set the active photo.
-     *
+     * <p/>
      * The photos metadata will be displayed in the inspector widget.
      *
      * @param photo The active photo for which to show further information
@@ -59,7 +68,7 @@ public class Inspector {
 
     @FXML private void initialize() {
         mapsScene = new GoogleMapsScene();
-
+        initializeTagList();
         // if placeholder is hidden then it should not take up any space
         placeholder.managedProperty().bind(placeholder.visibleProperty());
         // hide placeholder when details are visible
@@ -72,8 +81,73 @@ public class Inspector {
         setActivePhoto(null);
     }
 
+    /**
+     * Initialize tagList and tagListChangeListener.
+     * Do not yet add tagListChangeListener to tagList (this shall be done in
+     * method <tt>showCurrentlySetTags</tt>)
+     */
+    private void initializeTagList() {
+        ObservableList<Tag> tagNames = FXCollections.observableArrayList();
+        try {
+            for (Tag tag : photoservice.getAllTags()) {
+                tagNames.add(tag);
+            }
+        } catch (ServiceException ex) {
+            //TODO Dialog
+        }
+        tagList.setItems(tagNames);
+
+        tagList.setCellFactory(new Callback<ListView<Tag>, ListCell<Tag>>() {
+            @Override public ListCell<Tag> call(ListView<Tag> p) {
+                return new CheckBoxListCell<Tag>(item -> tagList.getItemBooleanProperty(item),
+                        new StringConverter<Tag>() {
+
+                            @Override public Tag fromString(String string) {
+                                return null;
+                            }
+
+                            @Override public String toString(Tag tag) {
+                                return tag.getName();
+                            }
+                        });
+            }
+        });
+
+        //initialize tagListChangeListener
+        tagListChangeListener = new ListChangeListener<Tag>() {
+            public void onChanged(ListChangeListener.Change<? extends Tag> c) {
+                while(c.next()) {
+                    List<Photo> photoList = new ArrayList<>();
+                    if (photo != null) {
+                        photoList.add(photo);
+                    }
+
+                    if (c.wasAdded()) {
+                        System.out.println("add " + c.getAddedSize());
+                        Tag added = c.getAddedSubList().get(0);
+                        try {
+                            photoservice.addTagToPhotos(photoList, added);
+                        } catch (ServiceException ex) {
+                            //TODO Dialog
+                        }
+                    }
+                    if (c.wasRemoved()) {
+                        System.out.println("remove " + c.getRemovedSize());
+                        Tag removed = c.getRemoved().get(0);
+                        try {
+                            photoservice.removeTagFromPhotos(photoList, removed);
+                        } catch (ServiceException ex) {
+                            //TODO Dialog
+                        }
+                    }
+                }
+            }
+        };
+    }
+
     private void handleDelete(Event event) {
-        if (photo == null) return;
+        if (photo == null)
+            return;
 
         List<Photo> photos = new ArrayList<>();
         photos.add(photo);
@@ -100,28 +174,41 @@ public class Inspector {
         }
 
         details.setVisible(true);
-
         mapsScene.addMarker(photo);
+        showCurrentlySetTags(photo);
 
         Exif exif = null;
         try {
             exif = exifService.getExif(photo);
-            ObservableList<Pair<String, String>> exifData = FXCollections.observableArrayList(
-                    new Pair<>("Aufnahmedatum", photo.getDate().toString()),
-                    new Pair<>("Kamerahersteller", exif.getMake()),
-                    new Pair<>("Kameramodell", exif.getModel()),
-                    new Pair<>("Belichtungszeit", exif.getExposure() + " Sek."),
-                    new Pair<>("Blende", "f/" + exif.getAperture()),
-                    new Pair<>("Brennweite", "" + exif.getFocalLength()),
-                    new Pair<>("ISO", "" + exif.getIso()),
-                    new Pair<>("Blitz", exif.isFlash()? "wurde ausgelöst" : "wurde nicht ausgelöst"),
-                    new Pair<>("Höhe", "" + exif.getAltitude()));
+            ObservableList<Pair<String, String>> exifData = FXCollections
+                    .observableArrayList(new Pair<>("Aufnahmedatum", photo.getDate().toString()),
+                            new Pair<>("Kamerahersteller", exif.getMake()),
+                            new Pair<>("Kameramodell", exif.getModel()),
+                            new Pair<>("Belichtungszeit", exif.getExposure() + " Sek."),
+                            new Pair<>("Blende", "f/" + exif.getAperture()),
+                            new Pair<>("Brennweite", "" + exif.getFocalLength()),
+                            new Pair<>("ISO", "" + exif.getIso()), new Pair<>("Blitz",
+                            exif.isFlash() ? "wurde ausgelöst" : "wurde nicht ausgelöst"),
+                            new Pair<>("Höhe", "" + exif.getAltitude()));
             exifName.setCellValueFactory(new PropertyValueFactory<>("Key"));
             exifValue.setCellValueFactory(new PropertyValueFactory<>("Value"));
             exifTable.setItems(exifData);
         } catch (ServiceException e) {
             //TODO Dialog
         }
+    }
 
+    private void showCurrentlySetTags(Photo photo) {
+        tagList.getCheckModel().getCheckedItems().removeListener(tagListChangeListener);
+        tagList.getCheckModel().clearChecks();
+        try {
+            List<Tag> currentTags = photoservice.getTagsForPhoto(photo);
+            for (Tag tag : currentTags) {
+                tagList.getCheckModel().check(tag);
+            }
+        } catch (ServiceException ex) {
+            //TODO Dialog
+        }
+        tagList.getCheckModel().getCheckedItems().addListener(tagListChangeListener);
     }
 }
