16094a0de43e323aebdda9cff6e844e5a2fc4cd1
Enri Miho <enrimiho@gmail.com>
2015-05-20 14:12:13 +0200

    Drop a pin on doubleclick on the map.

--- src/main/java/at/ac/tuwien/qse/sepm/gui/dialogs/FlickrDialog.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/dialogs/FlickrDialog.java
@@ -2,10 +2,18 @@ package at.ac.tuwien.qse.sepm.gui.dialogs;
 
 import at.ac.tuwien.qse.sepm.entities.Photo;
 import at.ac.tuwien.qse.sepm.gui.FXMLLoadHelper;
+import at.ac.tuwien.qse.sepm.gui.GoogleMapsScene;
 import at.ac.tuwien.qse.sepm.service.FlickrService;
 import at.ac.tuwien.qse.sepm.service.ServiceException;
 import at.ac.tuwien.qse.sepm.service.impl.FlickrServiceImpl;
 import at.ac.tuwien.qse.sepm.util.ErrorHandler;
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.event.UIEventType;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.Marker;
+import com.lynden.gmapsfx.javascript.object.MarkerOptions;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -14,7 +22,9 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
+import netscape.javascript.JSObject;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
@@ -28,16 +38,41 @@ public class FlickrDialog extends Dialog {
     @FXML private ProgressBar progressBar;
     @FXML private TilePane tilePane;
     @FXML private Button button;
+    @FXML private Pane mapContainer;
+    private GoogleMapView mapView;
+    private GoogleMap googleMap;
+    private Marker actualMarker;
 
     @Autowired private FlickrService flickrService;
 
     public FlickrDialog(Node origin, String title) {
         super(origin, title);
         FXMLLoadHelper.load(this, this, FlickrDialog.class, "view/FlickrDialog.fxml");
-        progress.setVisible(true);
+        GoogleMapsScene mapsScene = new GoogleMapsScene();
+        this.mapView = mapsScene.getMapView();
+        mapContainer.getChildren().add(mapsScene.getMapView());
         flickrService = new FlickrServiceImpl();
-        loadPhotos();
 
+        mapView.addMapInializedListener(new MapComponentInitializedListener() {
+            @Override
+            public void mapInitialized() {
+                //wait for the map to initialize.
+                googleMap = mapView.getMap();
+                googleMap.addUIEventHandler(UIEventType.dblclick, (JSObject obj) -> {
+                    //googleMap.setZoom(googleMap.getZoom()-1); //workaround to prevent zoom on doubleclick
+                    dropMarker(new LatLong((JSObject) obj.getMember("latLng")));
+                });
+
+            }
+        });
+
+    }
+
+    private void dropMarker(LatLong ll){
+        if(actualMarker!=null){
+            googleMap.removeMarker(actualMarker);
+        }
+        googleMap.addMarker(actualMarker = new Marker(new MarkerOptions().position(ll)));
     }
 
     @FXML
