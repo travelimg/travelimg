ef32a8ffebc263ee0bf6e353547809b7c8e0b602
David Peherstorfer <david.peherstorfer@live.de>
2015-05-27 09:05:37 +0200

    Change exif presentation

--- src/main/java/at/ac/tuwien/qse/sepm/gui/Inspector.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/Inspector.java
@@ -270,7 +270,7 @@ public class Inspector {
             try {
                 Exif exif = exifService.getExif(photo);
                 ObservableList<Pair<String, String>> exifData = FXCollections.observableArrayList(
-                        new Pair<>("Aufnahmedatum", photo.getDatetime().toString()), new Pair<>("Kamerahersteller", exif.getMake()),
+                        new Pair<>("Aufnahmedatum", photo.getDatetime().toString().replace("T", " ")), new Pair<>("Kamerahersteller", exif.getMake()),
                         new Pair<>("Kameramodell", exif.getModel()), new Pair<>("Belichtungszeit", exif.getExposure() + " Sek."),
                         new Pair<>("Blende", "f/" + exif.getAperture()), new Pair<>("Brennweite", "" + exif.getFocalLength()),
                         new Pair<>("ISO", "" + exif.getIso()), new Pair<>("Blitz",
