48fe052c5f38b0c2231048933454d37686c5424b
Enri Miho <enrimiho@gmail.com>
2015-06-04 13:37:30 +0200

    HighlightsViewController: location history playable

--- src/main/java/at/ac/tuwien/qse/sepm/gui/HighlightsViewController.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/HighlightsViewController.java
@@ -13,87 +13,47 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
+import java.util.ArrayList;
+
 public class HighlightsViewController {
 
     @FXML private BorderPane borderPane;
     @FXML private GoogleMapsScene mapsScene;
     private GoogleMapView mapView;
     private GoogleMap googleMap;
+    private ArrayList<Marker> markers = new ArrayList<Marker>();
+    private ArrayList<Polyline> polylines = new ArrayList<Polyline>();
+    private Marker actualMarker;
+    private int pos = 0;
 
     public void initialize(){
         //this is just for testing atm
+
         HBox vBox = new HBox();
         Button drawButton = new Button("draw polyline!");
         drawButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
             @Override
             public void handle(MouseEvent event) {
-                drawDestinationsAsPolyline();
+                clearMap();
+                drawDestinationsAsPolyline(getDestinations());
             }
         });
         Button playButton = new Button("play!");
+        playButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                if(pos==0){
+                    clearMap();
+                }
+                playTheJourney(pos);
+                pos++;
+            }
+        });
         vBox.getChildren().add(drawButton);
         vBox.getChildren().add(playButton);
         borderPane.setBottom(vBox);
-    }
-
-    private void drawDestinationsAsPolyline(){
-        //TODO note: this method will expect a list of destinations.
-        System.out.println("draw");
-        LatLong[] path={
-                new LatLong(48.2363038,16.3478819),
-                new LatLong( 48.236299,16.3478708),
-                new LatLong(48.232022, 16.376037),
-                new LatLong(48.216240, 16.396758),
-                new LatLong(48.197900, 16.415591),
-                new LatLong(48.189453, 16.403740),
-                new LatLong(48.185354, 16.362832),
-                new LatLong(48.194362, 16.343365),
-                new LatLong(48.215007, 16.338477),
-                new LatLong(48.161529, 16.369028)
-        };
-        PolylineOptions polylineOptions = new PolylineOptions();
-        polylineOptions.path(new MVCArray(path))
-                .clickable(false)
-                .draggable(false)
-                .editable(false)
-                .strokeColor("#ff4500")
-                .strokeWeight(2)
-                .visible(true);
-        Polyline polyline = new Polyline(polylineOptions);
-        googleMap.addMapShape(polyline);
-        Double ne_lat = null;
-        Double ne_long = null;
-        Double sw_lat = null;
-        Double sw_long = null;
-        for(int i = 0; i<path.length;i++) {
-            googleMap.addMarker(new Marker(new MarkerOptions().position(path[i]).icon("https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle_blue.png")));
-            if(ne_lat==null){
-                ne_lat = path[i].getLatitude();
-            }
-            if(ne_long==null){
-                ne_long = path[i].getLongitude();
-            }
-            if(sw_lat==null){
-                sw_lat = path[i].getLatitude();
-            }
-            if(sw_long==null){
-                sw_long = path[i].getLongitude();
-            }
-            if(path[i].getLatitude()>ne_lat){
-                ne_lat = path[i].getLatitude();
-            }
-            if(path[i].getLongitude()>ne_long){
-                ne_long = path[i].getLongitude();
-            }
-            if(path[i].getLatitude()<sw_lat){
-                sw_lat = path[i].getLatitude();
-            }
-            if(path[i].getLongitude()<sw_long){
-                sw_long = path[i].getLongitude();
-            }
-        }
-
-        fitMarkersToScreen(new LatLong(ne_lat,ne_long),new LatLong(sw_lat,sw_long));
     }
 
     public void setMap(GoogleMapsScene map) {
@@ -109,7 +69,94 @@ public class HighlightsViewController {
         borderPane.setTop(map.getMapView());
     }
 
-    private void fitMarkersToScreen(LatLong ne, LatLong sw) {
+    private void drawDestinationsAsPolyline(LatLong[] path){
+        //TODO note: this method will expect a list of destinations.
+
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.path(new MVCArray(path))
+                .clickable(false)
+                .draggable(false)
+                .editable(false)
+                .strokeColor("#ff4500")
+                .strokeWeight(2)
+                .visible(true);
+        Polyline polyline = new Polyline(polylineOptions);
+        googleMap.addMapShape(polyline);
+        polylines.add(polyline);
+        fitMarkersToScreen(path, 0, path.length - 1);
+        for(int i = 0; i<path.length;i++) {
+            Marker m = new Marker(new MarkerOptions().position(path[i]).icon("https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle_blue.png"));
+            googleMap.addMarker(m);
+            markers.add(m);
+        }
+    }
+
+    private void playTheJourney(int pos) {
+
+        LatLong [] path = getDestinations();
+
+        fitMarkersToScreen(path, pos, pos+1);
+        if(actualMarker!=null){
+            googleMap.removeMarker(actualMarker);
+        }
+        Marker m = new Marker(new MarkerOptions().position(path[pos]).icon("https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle_blue.png"));
+        googleMap.addMarker(m);
+        markers.add(m);
+        if (pos < path.length - 1) {
+
+            PolylineOptions polylineOptions = new PolylineOptions();
+            MVCArray mvcArray = new MVCArray();
+            mvcArray.push(path[pos]);
+            mvcArray.push(path[pos + 1]);
+            polylineOptions.path(mvcArray)
+                    .clickable(false)
+                    .draggable(false)
+                    .editable(false)
+                    .strokeColor("#ff4500")
+                    .strokeWeight(2)
+                    .visible(true);
+            Polyline polyline = new Polyline(polylineOptions);
+            googleMap.addMapShape(polyline);
+            polylines.add(polyline);
+            actualMarker = new Marker(new MarkerOptions().position(path[pos+1]));
+            googleMap.addMarker(actualMarker);
+        }
+
+    }
+
+    private void fitMarkersToScreen(LatLong[] subpath, int from, int to) {
+        Double ne_lat = null;
+        Double ne_long = null;
+        Double sw_lat = null;
+        Double sw_long = null;
+        for(int i = from; i<=to && i<subpath.length;i++) {
+            if (ne_lat == null) {
+                ne_lat = subpath[i].getLatitude();
+            }
+            if (ne_long == null) {
+                ne_long = subpath[i].getLongitude();
+            }
+            if (sw_lat == null) {
+                sw_lat = subpath[i].getLatitude();
+            }
+            if (sw_long == null) {
+                sw_long = subpath[i].getLongitude();
+            }
+            if (subpath[i].getLatitude() > ne_lat) {
+                ne_lat = subpath[i].getLatitude();
+            }
+            if (subpath[i].getLongitude() > ne_long) {
+                ne_long = subpath[i].getLongitude();
+            }
+            if (subpath[i].getLatitude() < sw_lat) {
+                sw_lat = subpath[i].getLatitude();
+            }
+            if (subpath[i].getLongitude() < sw_long) {
+                sw_long = subpath[i].getLongitude();
+            }
+        }
+        LatLong ne = new LatLong(ne_lat,ne_long);
+        LatLong sw = new LatLong(sw_lat,sw_long);
         double latFraction = ((ne.latToRadians()) - sw.latToRadians()) / Math.PI;
         double lngDiff = ne.getLongitude() - sw.getLongitude();
         double lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
@@ -119,5 +166,36 @@ public class HighlightsViewController {
         min = Math.min(min,21);
         mapsScene.setZoom((int)min);
         mapsScene.setCenter((ne.getLatitude()+sw.getLatitude())/2,(ne.getLongitude()+sw.getLongitude())/2);
+    }
+
+    private LatLong[] getDestinations(){
+        LatLong[] path={
+                new LatLong(48.2363038,16.3478819),
+                new LatLong( 48.236299,16.3478708),
+                new LatLong(48.232022, 16.376037),
+                new LatLong(48.216240, 16.396758),
+                new LatLong(48.197900, 16.415591),
+                new LatLong(48.189453, 16.403740),
+                new LatLong(48.185354, 16.362832),
+                new LatLong(48.194362, 16.343365),
+                new LatLong(48.215007, 16.338477),
+                new LatLong(48.161529, 16.369028)
+        };
+        return path;
+    }
+
+    private void clearMap(){
+        for(Marker m : markers){
+            googleMap.removeMarker(m);
+        }
+        markers.clear();
+        for(Polyline p : polylines){
+            googleMap.removeMapShape(p);
+        }
+        polylines.clear();
+        pos = 0;
+        if(actualMarker!=null){
+            googleMap.removeMarker(actualMarker);
+        }
     }
 }
