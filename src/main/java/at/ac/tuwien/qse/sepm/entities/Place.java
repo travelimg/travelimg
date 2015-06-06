137e57f6abe41ecb69b3797f6e57e1ab069ec176
Enri Miho <enrimiho@gmail.com>
2015-06-07 01:19:26 +0200

    Place entity: add latitude and longitude as attributes.

--- src/main/java/at/ac/tuwien/qse/sepm/entities/Place.java
+++ src/main/java/at/ac/tuwien/qse/sepm/entities/Place.java
@@ -5,6 +5,8 @@ public class Place {
     private int id;
     private String city;
     private String country;
+    private double latitude;
+    private double longitude;
 
     public Place(int id, String city, String country) {
         this.id = id;
@@ -36,11 +38,29 @@ public class Place {
         this.country = country;
     }
 
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
     @Override
     public String toString() {
         return "Place{" +
                 "city='" + city + '\'' +
                 ", country='" + country + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
                 '}';
     }
 }
