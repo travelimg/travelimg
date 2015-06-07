e2195e8761a4b25240d2081e4deff99cfbe05d9a
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-06-07 22:10:06 +0200

    Style fixes and add error dialog to journey creation

--- src/main/java/at/ac/tuwien/qse/sepm/gui/dialogs/JourneyDialog.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/dialogs/JourneyDialog.java
@@ -13,29 +13,20 @@ import javafx.scene.control.TextField;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * Created by David on 21.05.2015.
- */
-public class JourneyDialog extends ResultDialog<Journey>{
+public class JourneyDialog extends ResultDialog<Journey> {
 
     private static final Logger logger = LogManager.getLogger(JourneyDialog.class);
 
     private final ClusterService clusterService;
 
     @FXML private Button journeyAdd;
-
     @FXML private TextField journeyNameField;
-
     @FXML private DatePicker journeyEndDate;
-
     @FXML private Button journeyCancel;
-
     @FXML private DatePicker journeyBeginDate;
-
 
     public JourneyDialog(Node origin, ClusterService clusterService) {
         super(origin, "Reise hinzufügen");
-        logger.debug("new Journey Dialog");
         FXMLLoadHelper.load(this, this, JourneyDialog.class, "view/JourneyDialog.fxml");
 
         this.clusterService = clusterService;
@@ -52,12 +43,12 @@ public class JourneyDialog extends ResultDialog<Journey>{
         Journey journey = new Journey(-1, journeyNameField.getText(), journeyBeginDate.getValue().atStartOfDay(), journeyEndDate.getValue().atStartOfDay());
         try {
             clusterService.clusterJourney(journey);
-        } catch (ServiceException e) {
-            e.printStackTrace();
-            // TODO: Show Error Dialog
+        } catch (ServiceException ex) {
+            logger.error("Failed to cluster journey", ex);
+            ErrorDialog.show(getParent(), "Fehler beim Erstellen der Reise", "Fehlermeldung: " + ex.getMessage());
         }
 
-        //setResult(journey);
+        setResult(journey);
         close();
     }
 }
