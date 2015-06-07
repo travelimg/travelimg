62632b09993ac1789c3c1c9f9bfd5f6ee2e2453b
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-06-07 12:17:35 +0200

    Remove unused test methods

--- src/test/java/at/ac/tuwien/qse/sepm/service/impl/PhotoServiceImplTest.java
+++ src/test/java/at/ac/tuwien/qse/sepm/service/impl/PhotoServiceImplTest.java
@@ -173,49 +173,6 @@ public class PhotoServiceImplTest {
 
     }
 
-
-    //void requestFullscreenMode(List<Photo> photos) throws ServiceException;
-    public void testRequestFullScreenMode() throws ServiceException{
-
-    }
-    /**
-     * Add Tag <tt>tag</tt> to every photo in list <tt>photos</tt>. If a photo already has this tag,
-     * then it will keep it.
-     *
-     * @param photos must not be null; all elements must not be null; no element.id must be null
-     * @param tag    must not be null; tag.id must not be null
-     * @throws ServiceException         if an Exception in this or an underlying
-     *                                  layer occurs
-     */
-    //void addTagToPhotos(List<Photo> photos, Tag tag) throws ServiceException;
-    public void testAddTagToPhotos() throws ServiceException{
-
-    }
-    /**
-     * Remove Tag <tt>tag</tt> from all photos in list <tt>photos</tt>. If a photo in the list
-     * does not have this tag, then no action will be taken for this photo.
-     *
-     * @param photos must not be null; all elements must not be null; no element.id must be null
-     * @param tag    must not be null; tag.id must not be null
-     * @throws ServiceException         if an Exception in this or an underlying
-     *                                  layer occurs
-     */
-    //void removeTagFromPhotos(List<Photo> photos, Tag tag) throws ServiceException;
-    public void testRemoveTagFromPhotos() throws ServiceException{
-
-    }
-    /**
-     * Return list of all tags which are currently set for <tt>photo</tt>.
-     *
-     * @param photo must not be null; photo.id must not be null;
-     * @return List with all tags which are linked to <tt>photo</tt> as a PhotoTag;
-     * If no tag exists, return an empty List.
-     * @throws ServiceException         if an exception occurs on this or an underlying layer
-     */
-    //List<Tag> getTagsForPhoto(Photo photo) throws ServiceException;
-    public void testGetTagsForPhoto() throws ServiceException{
-
-    }
     /**
      * Persists the rating of the specified photo.
      *
