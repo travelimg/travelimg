bea0c859c60251fcef01e25dd012c18ac6b5dd95
Christoph Wasylewski <e1226587@student.tuwien.ac.at>
2015-06-07 15:47:38 +0200

    modify getMostWanted() in TagServiceImpl.java; create new test test_getMostWantetWithNoTags() in TagTest.java

--- src/main/java/at/ac/tuwien/qse/sepm/service/impl/TagServiceImpl.java
+++ src/main/java/at/ac/tuwien/qse/sepm/service/impl/TagServiceImpl.java
@@ -12,9 +12,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TagServiceImpl implements TagService {
 
@@ -141,52 +140,29 @@ public class TagServiceImpl implements TagService {
     }
 
     @Override public List<Tag> getMostWanted(List<Photo> l) throws ServiceException {
-        LOGGER.debug("Entering getMostWanted with{}",l);
-        HashMap<Tag, Integer> tagcounter = new HashMap<>();
-        for (Photo p : l) {
-            for (Tag t : p.getTags()) {
-                if (tagcounter.size() == 0) {
-                    tagcounter.put(t, 1);
+
+        LOGGER.debug("Entering getMostWanted with{}", l);
+
+        HashMap<Tag, Integer> counter = new HashMap<>();
+        // count the frequency of each tag
+
+        for (Photo photo : l) {
+            for (Tag tag : photo.getTags()) {
+                if (counter.containsKey(tag)) {
+                    counter.put(tag, counter.get(tag) + 1);
                 } else {
-                    boolean neu = true;
-                    for (Tag ta : tagcounter.keySet()) {
-                        if (ta.getId() == t.getId()) {
-                            tagcounter.replace(ta, tagcounter.get(ta), tagcounter.get(ta) + 1);
-                            neu = false;
-                        }
-                    }
-                    if (neu)
-                        tagcounter.put(t, 1);
-
+                    counter.put(tag, 1);
                 }
             }
         }
-        LOGGER.debug("Count Tags per Photo",tagcounter);
-        List<Tag> returnList = new ArrayList<>();
-
-        for (int i = 0; i < 5; i++) {
-            Tag tag = null;
-            if (tagcounter.size() != 0) {
-                for (Tag t : tagcounter.keySet()) {
-                    if (tag == null) {
-                        tag = t;
-                    } else {
-                        if (tagcounter.get(tag) < tagcounter.get(t)) {
-                            tag = t;
-                        }
-                    }
-                }
-                returnList.add(tag);
-                tagcounter.remove(tag);
-            }
+        if(counter.size()==0){
+            throw new ServiceException("No Tags found");
         }
-
-        if(returnList.size()==0){
-            throw  new ServiceException("No Tags found");
-        }
+        // return the most fequent tags
         LOGGER.debug("Leaving getMostWanted {}");
-        return returnList;
+        return counter.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).limit(5)
+                .map(Map.Entry::getKey).collect(Collectors.toList());
 
     }
-
 }
