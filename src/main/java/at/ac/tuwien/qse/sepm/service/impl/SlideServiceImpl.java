180ce542f13562c4931f93805ecda4cc8a625e67
mb <mb@mb.mb>
2015-06-08 11:52:59 +0200

    SlideServiceImpl created SlideshowServiceImpl created

---
+++ src/main/java/at/ac/tuwien/qse/sepm/service/impl/SlideServiceImpl.java
@@ -0,0 +1,54 @@
+package at.ac.tuwien.qse.sepm.service.impl;
+
+import at.ac.tuwien.qse.sepm.dao.DAOException;
+import at.ac.tuwien.qse.sepm.dao.SlideDAO;
+import at.ac.tuwien.qse.sepm.entities.Slide;
+import at.ac.tuwien.qse.sepm.entities.validators.ValidationException;
+import at.ac.tuwien.qse.sepm.service.ServiceException;
+import at.ac.tuwien.qse.sepm.service.SlideService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by mb on 08.06.15.
+ */
+public class SlideServiceImpl implements SlideService{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @Autowired
+    private SlideDAO slideDAO;
+
+    @Override public void create(Slide slide) throws ServiceException {
+        try
+        {
+            slideDAO.create(slide);
+
+        } catch( DAOException | ValidationException ex) {
+            throw new ServiceException("Failed to create Slide", ex);
+        }
+    }
+
+    @Override public void delete(Slide slide) throws ServiceException {
+
+        try
+        {
+            slideDAO.delete(slide);
+
+        } catch( DAOException | ValidationException ex) {
+            throw new ServiceException("Failed to delete slide", ex);
+        }
+    }
+
+    @Override public List<Slide> readAll() throws ServiceException {
+        LOGGER.debug("Retrieving all tags...");
+        try {
+            return slideDAO.readAll();
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+    }
+}
---
+++ src/main/java/at/ac/tuwien/qse/sepm/service/impl/SlideshowServiceImpl.java
@@ -0,0 +1,58 @@
+package at.ac.tuwien.qse.sepm.service.impl;
+
+import at.ac.tuwien.qse.sepm.dao.DAOException;
+import at.ac.tuwien.qse.sepm.dao.SlideshowDAO;
+import at.ac.tuwien.qse.sepm.entities.Slideshow;
+import at.ac.tuwien.qse.sepm.entities.validators.ValidationException;
+import at.ac.tuwien.qse.sepm.service.ServiceException;
+
+import at.ac.tuwien.qse.sepm.service.SlideshowService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by mb on 08.06.15.
+ */
+public class SlideshowServiceImpl implements SlideshowService {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @Autowired
+    private SlideshowDAO slideshowDAO;
+
+    @Override
+    public void create(Slideshow slideshow) throws ServiceException {
+
+        try {
+            slideshowDAO.create(slideshow);
+        } catch(DAOException | ValidationException ex) {
+            throw new ServiceException("Failed to create slideshow",ex);
+        }
+
+
+    }
+
+    @Override
+    public void delete(Slideshow slideshow) throws ServiceException {
+
+        try {
+            slideshowDAO.delete(slideshow);
+        } catch(DAOException | ValidationException ex) {
+            throw new ServiceException("Failed to delete slideshow",ex);
+        }
+    }
+
+    @Override
+    public List<Slideshow> getAllSlideshows() throws ServiceException {
+
+        try {
+            return slideshowDAO.readAll();
+        } catch(DAOException ex) {
+            throw new ServiceException("Failed to return a List of slideshows",ex);
+        }
+    }
+
+}
