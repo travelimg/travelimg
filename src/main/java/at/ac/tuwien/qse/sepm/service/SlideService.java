7e7015673cf66a048b136125de3a75670229b44f
mb <mb@mb.mb>
2015-06-08 11:40:51 +0200

    SlideService created SlideshowService created

---
+++ src/main/java/at/ac/tuwien/qse/sepm/service/SlideService.java
@@ -0,0 +1,32 @@
+package at.ac.tuwien.qse.sepm.service;
+
+import at.ac.tuwien.qse.sepm.entities.Slide;
+
+import java.util.List;
+
+/**
+ * Created by mb on 08.06.15.
+ */
+public interface SlideService {
+
+    /**
+     * Create a slide
+     * @param slide
+     * @throws ServiceException
+     */
+    void create(Slide slide) throws ServiceException;
+
+    /**
+     * Delete a slide
+     * @param slide
+     * @throws ServiceException
+     */
+    void delete(Slide slide) throws ServiceException;
+
+    /**
+     * Return all Slides
+     * @return
+     * @throws ServiceException
+     */
+    List<Slide> readAll() throws ServiceException;
+}
---
+++ src/main/java/at/ac/tuwien/qse/sepm/service/SlideshowService.java
@@ -0,0 +1,41 @@
+package at.ac.tuwien.qse.sepm.service;
+
+import at.ac.tuwien.qse.sepm.entities.Slideshow;
+
+import java.util.List;
+
+/**
+ * Created by mb on 08.06.15.
+ */
+public interface SlideshowService {
+
+    /**
+     * Create a Slideshow in the data store.
+     *
+     * @param slideshow  which to create; must not be null; must not already have an id
+     * @return the created slideshow
+     * @throws ServiceException If the Slideshow can not be created or the data store fails to
+     *      create a record.
+     */
+    void create(Slideshow slideshow) throws ServiceException;
+
+    /**
+     * Delete an existing Slideshow.
+     *
+     * @param slideshow Specifies which slideshow to delete by providing the id;
+     *            must not be null;
+     *            <tt>slideshow.id</tt> must not be null;
+     * @throws ServiceException If the Slideshwo can not be deleted or the data store fails to
+     *     delete the record.
+     */
+    void delete(Slideshow slideshow) throws ServiceException;
+
+    /**
+     * Return a list of all existing slideshows.
+     *
+     * @return the list of all available slideshows
+     * @throws ServiceException if retrieval failed
+     */
+    List<Slideshow> getAllSlideshows() throws ServiceException;
+
+}
