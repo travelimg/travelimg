ddc3875583bb8fd8976e7d011a0d010db64c7d71
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-06-21 11:35:20 +0200

    Implement rendering of title slide.

--- src/main/java/at/ac/tuwien/qse/sepm/gui/slide/TitleSlideView.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/slide/TitleSlideView.java
@@ -1,7 +1,11 @@
 package at.ac.tuwien.qse.sepm.gui.slide;
 
-import at.ac.tuwien.qse.sepm.entities.MapSlide;
 import at.ac.tuwien.qse.sepm.entities.TitleSlide;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Paint;
 
 public class TitleSlideView extends SlideView {
 
@@ -9,5 +13,13 @@ public class TitleSlideView extends SlideView {
 
     public TitleSlideView(TitleSlide slide) {
         this.slide = slide;
+
+        Background background = new Background(new BackgroundFill(Paint.valueOf("black"), null, null));
+        Label label = new Label(slide.getCaption());
+
+        setBackground(background);
+        getChildren().add(label);
+
+        setAlignment(label, Pos.CENTER);
     }
 }
