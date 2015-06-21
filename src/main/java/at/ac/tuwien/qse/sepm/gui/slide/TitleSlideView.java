586aeb216f3f37899e20799c7b11e44ea1164c18
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-06-21 13:07:28 +0200

    Set the correct color in TitleSlideView

--- src/main/java/at/ac/tuwien/qse/sepm/gui/slide/TitleSlideView.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/slide/TitleSlideView.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.qse.sepm.gui.slide;
 
 import at.ac.tuwien.qse.sepm.entities.TitleSlide;
+import at.ac.tuwien.qse.sepm.gui.util.ColorUtils;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
@@ -15,7 +16,7 @@ public class TitleSlideView extends SlideView {
     public TitleSlideView(TitleSlide slide) {
         this.slide = slide;
 
-        Background background = new Background(new BackgroundFill(Paint.valueOf("black"), null, null));
+        Background background = new Background(new BackgroundFill(ColorUtils.fromInt(slide.getColor()), null, null));
         Label label = new Label(slide.getCaption());
 
         setBackground(background);
