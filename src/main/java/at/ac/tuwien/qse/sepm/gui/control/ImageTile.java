69238ef81d92bb72a6f2c333a310ac2e4dc4ebfc
Enri Miho <enrimiho@gmail.com>
2015-06-23 13:24:02 +0200

    Create class ImageTile

---
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/control/ImageTile.java
@@ -0,0 +1,23 @@
+package at.ac.tuwien.qse.sepm.gui.control;
+
+import at.ac.tuwien.qse.sepm.entities.Photo;
+import at.ac.tuwien.qse.sepm.entities.Tag;
+import javafx.scene.layout.StackPane;
+
+import java.util.List;
+
+public class ImageTile extends StackPane {
+
+    public ImageTile(){
+        //TODO
+    }
+
+    public void setPhotos(List<Photo> photos){
+        //TODO
+    }
+
+    public void setTag(Tag tag){
+        //TODO
+    }
+
+}
