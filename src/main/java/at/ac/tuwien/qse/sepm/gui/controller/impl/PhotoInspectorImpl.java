84a13ec1b00b0641ea3b4f0c79369519d5e3971e
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2015-06-24 17:15:50 +0200

    Clean up PhotoInspectorImpl.

--- src/main/java/at/ac/tuwien/qse/sepm/gui/controller/impl/PhotoInspectorImpl.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/controller/impl/PhotoInspectorImpl.java
@@ -6,7 +6,6 @@ import at.ac.tuwien.qse.sepm.gui.controller.SlideshowView;
 import at.ac.tuwien.qse.sepm.gui.dialogs.ErrorDialog;
 import at.ac.tuwien.qse.sepm.gui.util.LatLong;
 import at.ac.tuwien.qse.sepm.service.*;
-import javafx.collections.ListChangeListener;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -46,8 +45,6 @@ public class PhotoInspectorImpl extends InspectorImpl<Photo> {
     @FXML
     private GoogleMapScene mapScene;
 
-    private Runnable updateHandler;
-
     @Autowired
     private SlideshowView slideshowView;
 
@@ -62,128 +59,17 @@ public class PhotoInspectorImpl extends InspectorImpl<Photo> {
 
         getEntities().forEach(photo -> mapScene.addMarker(new LatLong(photo.getData().getLatitude(), photo.getData().getLongitude())));
 
-        showDetails(getEntities());
+        update(getEntities());
     }
 
     @FXML
     private void initialize() {
-        ratingPicker.setRatingChangeHandler(this::handleRatingChange);
         addToSlideshowButton.setOnAction(this::handleAddToSlideshow);
         slideshowsCombobox.setConverter(new SlideshowStringConverter());
         slideshowsCombobox.setItems(slideshowView.getSlideshows());
 
-        tagPicker.setOnUpdate(() -> getEntities().forEach(this::updateTags));
-    }
-
-    private void handleRatingChange(Rating newRating) {
-        LOGGER.debug("rating picker changed to {}", newRating);
-
-        Collection<Photo> photos = getEntities();
-        if (photos.size() == 0) {
-            LOGGER.debug("No photo selected.");
-            return;
-        }
-
-        for (Photo photo : photos) {
-
-            if (photo.getData().getRating() == newRating) {
-                LOGGER.debug("photo already has rating of {}", newRating);
-                continue;
-            }
-
-            Rating oldRating = photo.getData().getRating();
-            LOGGER.debug("setting photo rating from {} to {}", oldRating, newRating);
-            photo.getData().setRating(newRating);
-
-            try {
-                photoservice.editPhoto(photo);
-            } catch (ServiceException ex) {
-                LOGGER.error("Failed saving photo rating.", ex);
-                LOGGER.debug("Resetting rating from {} to {}.", newRating, oldRating);
-
-                // Undo changes.
-                photo.getData().setRating(oldRating);
-                // FIXME: Reset the RatingPicker.
-                // This is not as simple as expected. Calling ratingPicker.getData().setRating(oldValue) here
-                // will complete and finish. But once the below dialog is closed ANOTHER selection-
-                // change will occur in RatingPicker that is the same as the once that caused the error.
-                // That causes an infinite loop of error dialogs.
-
-                ErrorDialog.show(root,
-                        "Bewertung fehlgeschlagen",
-                        "Die Bewertung für das Foto konnte nicht gespeichert werden."
-                );
-            }
-        }
-
-        onUpdate();
-    }
-
-    private void showDetails(Collection<Photo> photos) {
-
-        // Depending on the number of photos selected show different elements.
-        boolean noneActive = photos.size() == 0;
-        boolean singleActive = photos.size() == 1;
-
-        // Nothing to show.
-        if (noneActive) return;
-
-        // Only set a non-null rating if all active photos have the same rating.
-        // Otherwise the rating picker should be indetermined.
-        ratingPicker.setRating(null);
-        List<Rating> ratings = photos.stream()
-                .map(photo -> photo.getData().getRating())
-                .distinct()
-                .collect(Collectors.toList());
-        if (ratings.size() == 1) {
-            ratingPicker.setRating(ratings.get(0));
-        }
-
-        // Add the map markers for each photo.
-        mapScene.clear();
-        photos.forEach((photo) -> mapScene.addMarker(new LatLong(photo.getData().getLatitude(), photo.getData().getLongitude())));
-        mapScene.fitToMarkers();
-
-        // Add tags to the tag picker.
-        tagPicker.getEntities().clear();
-        photos.forEach((photo) -> {
-            Set<String> tags = photo.getData().getTags().stream()
-                    .map(Tag::getName)
-                    .collect(Collectors.toSet());
-            tagPicker.getEntities().add(tags);
-        });
-
-        // Fetch EXIF data for all photos.
-        Collection<Exif> exifs = new ArrayList<>(photos.size());
-        for (Photo photo : photos) {
-            try {
-                exifs.add(exifService.getExif(photo));
-            } catch (ServiceException ex) {
-                LOGGER.warn("failed loading EXIF data for photo {}", photo);
-                LOGGER.error("", ex);
-            }
-        }
-
-        exifList.getChildren().clear();
-        addExifCell("Fotograf", photos, p -> {
-            Photographer photographer = p.getData().getPhotographer();
-            if (photographer == null)
-                return null;
-            return photographer.getName();
-        });
-        addExifCell("Datum", photos, p -> {
-            LocalDateTime date = p.getData().getDatetime();
-            if (date == null) return null;
-            return date.toString().replace("T", " ");
-        });
-        addExifCell("Kamerahersteller", exifs, Exif::getMake);
-        addExifCell("Kameramodell", exifs, Exif::getModel);
-        addExifCell("Belichtungszeit", exifs, e -> e.getExposure() + " Sekunden");
-        addExifCell("Blende", exifs, Exif::getAperture);
-        addExifCell("Brennweite", exifs, Exif::getFocalLength);
-        addExifCell("ISO", exifs, Exif::getIso);
-        addExifCell("Blitz", exifs, e -> e.isFlash() ? "ausgelöst" : "ohne");
-        addExifCell("Höhe", exifs, Exif::getAltitude);
+        ratingPicker.setRatingChangeHandler(r -> getEntities().forEach(this::saveRating));
+        tagPicker.setOnUpdate(() -> getEntities().forEach(this::saveTags));
     }
 
     private void handleAddToSlideshow(Event event) {
@@ -196,7 +82,40 @@ public class PhotoInspectorImpl extends InspectorImpl<Photo> {
         slideshowView.addPhotosToSlideshow(new ArrayList<>(getEntities()), slideshow);
     }
 
-    private void updateTags(Photo photo) {
+    private void saveRating(Photo photo) {
+        Rating rating = ratingPicker.getRating();
+        if (photo.getData().getRating() == rating) {
+            LOGGER.debug("photo already has rating of {}", rating);
+            return;
+        }
+
+        Rating oldRating = photo.getData().getRating();
+        LOGGER.debug("setting photo rating from {} to {}", oldRating, rating);
+        photo.getData().setRating(rating);
+
+        try {
+            photoservice.editPhoto(photo);
+        } catch (ServiceException ex) {
+            LOGGER.error("Failed saving photo rating.", ex);
+            LOGGER.debug("Resetting rating from {} to {}.", rating, oldRating);
+
+            // Undo changes.
+            photo.getData().setRating(oldRating);
+            // FIXME: Reset the RatingPicker.
+            // This is not as simple as expected. Calling ratingPicker.getData().setRating(oldValue) here
+            // will complete and finish. But once the below dialog is closed ANOTHER selection-
+            // change will occur in RatingPicker that is the same as the once that caused the error.
+            // That causes an infinite loop of error dialogs.
+
+            ErrorDialog.show(root,
+                    "Bewertung fehlgeschlagen",
+                    "Die Bewertung für das Foto konnte nicht gespeichert werden."
+            );
+        }
+
+        onUpdate();
+    }
+    private void saveTags(Photo photo) {
         LOGGER.debug("updating tags of {}", photo);
 
         // Find the new tags.
@@ -223,6 +142,72 @@ public class PhotoInspectorImpl extends InspectorImpl<Photo> {
         }
     }
 
+    private void update(Collection<Photo> photos) {
+        if (photos.size() == 0) return;
+        updateRatingPicker(photos);
+        updateMap(photos);
+        updateTagPicker(photos);
+        updateExifTable(photos);
+    }
+    private void updateRatingPicker(Collection<Photo> photos) {
+        ratingPicker.setRating(null);
+        List<Rating> ratings = photos.stream()
+                .map(photo -> photo.getData().getRating())
+                .distinct()
+                .collect(Collectors.toList());
+        if (ratings.size() == 1) {
+            ratingPicker.setRating(ratings.get(0));
+        }
+    }
+    private void updateMap(Collection<Photo> photos) {
+        mapScene.clear();
+        photos.forEach((photo) -> mapScene.addMarker(
+                new LatLong(photo.getData().getLatitude(), photo.getData().getLongitude())));
+        mapScene.fitToMarkers();
+    }
+    private void updateTagPicker(Collection<Photo> photos) {
+        tagPicker.getEntities().clear();
+        photos.forEach((photo) -> {
+            Set<String> tags = photo.getData().getTags().stream().map(Tag::getName)
+                    .collect(Collectors.toSet());
+            tagPicker.getEntities().add(tags);
+        });
+    }
+    private void updateExifTable(Collection<Photo> photos) {
+
+        // Fetch EXIF data for all photos.
+        Collection<Exif> exifs = new ArrayList<>(photos.size());
+        for (Photo photo : photos) {
+            try {
+                exifs.add(exifService.getExif(photo));
+            } catch (ServiceException ex) {
+                LOGGER.warn("failed loading EXIF data for photo {}", photo);
+                LOGGER.error("", ex);
+            }
+        }
+
+        exifList.getChildren().clear();
+        addExifCell("Fotograf", photos, p -> {
+            Photographer photographer = p.getData().getPhotographer();
+            if (photographer == null)
+                return null;
+            return photographer.getName();
+        });
+        addExifCell("Datum", photos, p -> {
+            LocalDateTime date = p.getData().getDatetime();
+            if (date == null)
+                return null;
+            return date.toString().replace("T", " ");
+        });
+        addExifCell("Kamerahersteller", exifs, Exif::getMake);
+        addExifCell("Kameramodell", exifs, Exif::getModel);
+        addExifCell("Belichtungszeit", exifs, e -> e.getExposure() + " Sekunden");
+        addExifCell("Blende", exifs, Exif::getAperture);
+        addExifCell("Brennweite", exifs, Exif::getFocalLength);
+        addExifCell("ISO", exifs, Exif::getIso);
+        addExifCell("Blitz", exifs, e -> e.isFlash() ? "ausgelöst" : "ohne");
+        addExifCell("Höhe", exifs, Exif::getAltitude);
+    }
     private <T> void addExifCell(String key, Collection<T> entities, Function<T, Object> mapper) {
         Set<String> values = new HashSet<>();
         entities.forEach(t -> {
