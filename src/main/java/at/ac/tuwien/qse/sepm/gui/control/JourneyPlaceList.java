529b0d604454b9a366d7321036d0b7da0d938381
Enri Miho <enrimiho@gmail.com>
2015-06-24 03:24:24 +0200

    Trying to connect radio buttons to each other (just for testing)

--- src/main/java/at/ac/tuwien/qse/sepm/gui/control/JourneyPlaceList.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/control/JourneyPlaceList.java
@@ -6,17 +6,21 @@ import at.ac.tuwien.qse.sepm.gui.FXMLLoadHelper;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.RectangleBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -45,6 +49,7 @@ public class JourneyPlaceList extends VBox {
     private Consumer<Journey> journeySelectedCallback = null;
     private Consumer<Place> placeSelectedCallback = null;
     private Consumer<Journey> allPlacesSelectedCallback = null;
+    private ToggleGroup toggleGroup = new ToggleGroup();
 
     public JourneyPlaceList() {
         FXMLLoadHelper.load(this, this, JourneyPlaceList.class, "view/control/JourneyPlaceList.fxml");
@@ -132,12 +137,35 @@ public class JourneyPlaceList extends VBox {
 
     private void populatePlacesBox() {
         places.getChildren().clear();
-
+        places.setSpacing(20.0);
+        places.setPadding(new Insets(10.0,0,0,10.0));
         places.getChildren().add(new AllPlacesEntry());
-        places.getChildren().addAll(placesByJourney.get(selectedJourney.get()).stream()
+
+
+
+        /*places.getChildren().addAll(placesByJourney.get(selectedJourney.get()).stream()
                         .map(PlaceEntry::new)
                         .collect(Collectors.toList())
-        );
+        );*/
+        List<Place> placesOfJourney = placesByJourney.get(selectedJourney.get());
+        VBox vbox2 = new VBox();
+        for(int i = 0; i<placesOfJourney.size(); i++ ){
+            vbox2.getChildren().add(new PlaceEntry(placesOfJourney.get(i)));
+            if(i!=placesOfJourney.size()-1){
+                Rectangle rect = RectangleBuilder.create()
+                        .width(5)
+                        .height(40)
+                        .styleClass("rect")
+                        .build();
+                VBox vbox = new VBox();
+                vbox.setPadding(new Insets(0,0,0,7.0));
+                vbox.getChildren().add(rect);
+                vbox2.getChildren().add(vbox);
+            }
+
+        }
+        places.getChildren().add(vbox2);
+
     }
 
     private class JourneyEntry extends Button {
@@ -148,18 +176,22 @@ public class JourneyPlaceList extends VBox {
         }
     }
 
-    private class AllPlacesEntry extends Button {
+    private class AllPlacesEntry extends RadioButton {
         public AllPlacesEntry() {
             super("Alle Orte");
-
+            setStyle("-fx-font-size: 15");
+            setToggleGroup(toggleGroup);
+            selectedProperty().setValue(true);
             setOnAction((event) -> allPlacesSelectedCallback.accept(selectedJourney.get()));
         }
     }
 
-    private class PlaceEntry extends Button {
+    private class PlaceEntry extends RadioButton {
         public PlaceEntry(Place place) {
             super(place.getCity());
-
+            setStyle("-fx-font-size: 15");
+            getStyleClass().add("place-radio-button");
+            setToggleGroup(toggleGroup);
             setOnAction((event) -> selectedPlace.set(place));
         }
     }
