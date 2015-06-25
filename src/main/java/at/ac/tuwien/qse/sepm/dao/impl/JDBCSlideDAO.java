0b7b427336ee51ed82b84c3f90e6b4b5ae9cae4a
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-06-25 22:25:34 +0200

    Fix code style

--- src/main/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCSlideDAO.java
+++ src/main/java/at/ac/tuwien/qse/sepm/dao/impl/JDBCSlideDAO.java
@@ -3,10 +3,8 @@ package at.ac.tuwien.qse.sepm.dao.impl;
 import at.ac.tuwien.qse.sepm.dao.DAOException;
 import at.ac.tuwien.qse.sepm.dao.PhotoDAO;
 import at.ac.tuwien.qse.sepm.dao.SlideDAO;
-import at.ac.tuwien.qse.sepm.dao.SlideshowDAO;
 import at.ac.tuwien.qse.sepm.entities.*;
 import at.ac.tuwien.qse.sepm.entities.validators.SlideValidator;
-import at.ac.tuwien.qse.sepm.entities.validators.SlideshowValidator;
 import at.ac.tuwien.qse.sepm.entities.validators.ValidationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -33,29 +31,22 @@ public class JDBCSlideDAO extends JDBCDAOBase implements SlideDAO {
     private static final String MAP_READ_ALL_BY_SLIDESHOW_STATEMENT = "SELECT Slide.id, slideshow_id, orderposition, caption, latitude, longitude, zoomLevel FROM MapSlide JOIN Slide ON MapSlide.id = Slide.id WHERE slideshow_id=? ORDER BY orderposition asc;";
     private static final String TITLE_INSERT_STATEMENT = "INSERT INTO TitleSlide(id, color) VALUES(?, ?)";
     private static final String TITLE_UPDATE_STATEMENT = "UPDATE TitleSlide SET color = ?  WHERE id = ?;";
-    private static final String TITLE_READ_ALL_BY_SLIDESHOW_STATEMENT =
-            "SELECT Slide.id, slideshow_id, orderposition, caption, color FROM TitleSlide JOIN Slide ON TitleSlide.id = Slide.id WHERE slideshow_id=? ORDER BY orderposition asc;";
+    private static final String TITLE_READ_ALL_BY_SLIDESHOW_STATEMENT = "SELECT Slide.id, slideshow_id, orderposition, caption, color FROM TitleSlide JOIN Slide ON TitleSlide.id = Slide.id WHERE slideshow_id=? ORDER BY orderposition asc;";
     private static final String PHOTO_INSERT_STATEMENT = "INSERT INTO PhotoSlide(id, photo_id) VALUES(?, ?)";
     private static final String PHOTO_UPDATE_STATEMENT = "UPDATE PhotoSlide SET photo_id = ?  WHERE id = ?;";
-    private static final String PHOTO_READ_ALL_BY_SLIDESHOW_STATEMENT =
-            "SELECT Slide.id, slideshow_id, orderposition, caption, photo_id FROM PhotoSlide JOIN Slide ON PhotoSlide.id = Slide.id WHERE slideshow_id=? ORDER BY orderposition asc;";
+    private static final String PHOTO_READ_ALL_BY_SLIDESHOW_STATEMENT = "SELECT Slide.id, slideshow_id, orderposition, caption, photo_id FROM PhotoSlide JOIN Slide ON PhotoSlide.id = Slide.id WHERE slideshow_id=? ORDER BY orderposition asc;";
 
-    @Autowired
-    private PhotoDAO photoDAO;
+    @Autowired private PhotoDAO photoDAO;
 
     private SimpleJdbcInsert insertSlide;
 
-    @Override
-    @Autowired
-    public void setDataSource(DataSource dataSource) {
+    @Override @Autowired public void setDataSource(DataSource dataSource) {
         super.setDataSource(dataSource);
-        this.insertSlide = new SimpleJdbcInsert(dataSource)
-                .withTableName("Slide")
+        this.insertSlide = new SimpleJdbcInsert(dataSource).withTableName("Slide")
                 .usingGeneratedKeyColumns("id");
     }
 
-    @Override
-    public PhotoSlide create(PhotoSlide slide) throws DAOException, ValidationException {
+    @Override public PhotoSlide create(PhotoSlide slide) throws DAOException, ValidationException {
         createBase(slide);
 
         try {
@@ -66,8 +57,7 @@ public class JDBCSlideDAO extends JDBCDAOBase implements SlideDAO {
         }
     }
 
-    @Override
-    public MapSlide create(MapSlide slide) throws DAOException, ValidationException {
+    @Override public MapSlide create(MapSlide slide) throws DAOException, ValidationException {
         createBase(slide);
 
         try {
@@ -79,8 +69,7 @@ public class JDBCSlideDAO extends JDBCDAOBase implements SlideDAO {
         }
     }
 
-    @Override
-    public TitleSlide create(TitleSlide slide) throws DAOException, ValidationException {
+    @Override public TitleSlide create(TitleSlide slide) throws DAOException, ValidationException {
         createBase(slide);
 
         try {
@@ -91,17 +80,13 @@ public class JDBCSlideDAO extends JDBCDAOBase implements SlideDAO {
         }
     }
 
-    @Override
-    public PhotoSlide update(PhotoSlide slide) throws DAOException, ValidationException {
+    @Override public PhotoSlide update(PhotoSlide slide) throws DAOException, ValidationException {
         logger.debug("Updating photo slide {}", slide);
 
         updateBase(slide);
 
         try {
-            jdbcTemplate.update(PHOTO_UPDATE_STATEMENT,
-                    slide.getPhoto().getId(),
-                    slide.getId()
-            );
+            jdbcTemplate.update(PHOTO_UPDATE_STATEMENT, slide.getPhoto().getId(), slide.getId());
             logger.debug("Successfully updated photo slide");
             return slide;
         } catch (DataAccessException ex) {
@@ -110,8 +95,7 @@ public class JDBCSlideDAO extends JDBCDAOBase implements SlideDAO {
         }
     }
 
-    @Override
-    public MapSlide update(MapSlide slide) throws DAOException, ValidationException {
+    @Override public MapSlide update(MapSlide slide) throws DAOException, ValidationException {
         logger.debug("Updating map slide {}", slide);
 
         updateBase(slide);
@@ -127,17 +111,13 @@ public class JDBCSlideDAO extends JDBCDAOBase implements SlideDAO {
         }
     }
 
-    @Override
-    public TitleSlide update(TitleSlide slide) throws DAOException, ValidationException {
+    @Override public TitleSlide update(TitleSlide slide) throws DAOException, ValidationException {
         logger.debug("Updating title slide {}", slide);
 
         updateBase(slide);
 
         try {
-            jdbcTemplate.update(TITLE_UPDATE_STATEMENT,
-                    slide.getColor(),
-                    slide.getId()
-            );
+            jdbcTemplate.update(TITLE_UPDATE_STATEMENT, slide.getColor(), slide.getId());
             logger.debug("Successfully updated title slide");
             return slide;
         } catch (DataAccessException ex) {
@@ -146,43 +126,42 @@ public class JDBCSlideDAO extends JDBCDAOBase implements SlideDAO {
         }
     }
 
-    @Override
-    public void delete(PhotoSlide slide) throws DAOException, ValidationException {
+    @Override public void delete(PhotoSlide slide) throws DAOException, ValidationException {
 
     }
 
-    @Override
-    public void delete(MapSlide slide) throws DAOException, ValidationException {
+    @Override public void delete(MapSlide slide) throws DAOException, ValidationException {
 
     }
 
-    @Override
-    public void delete(TitleSlide slide) throws DAOException, ValidationException {
+    @Override public void delete(TitleSlide slide) throws DAOException, ValidationException {
 
     }
 
-    @Override
-    public List<PhotoSlide> getPhotoSlidesForSlideshow(int slideshowId) throws DAOException {
+    @Override public List<PhotoSlide> getPhotoSlidesForSlideshow(int slideshowId)
+            throws DAOException {
         try {
-            return jdbcTemplate.query(PHOTO_READ_ALL_BY_SLIDESHOW_STATEMENT, new PhotoSlideMapper(), slideshowId);
+            return jdbcTemplate.query(PHOTO_READ_ALL_BY_SLIDESHOW_STATEMENT, new PhotoSlideMapper(),
+                    slideshowId);
         } catch (DataAccessException | DAOException.Unchecked ex) {
             throw new DAOException("Failed to retrieve photo slides for given slideshow", ex);
         }
     }
 
-    @Override
-    public List<MapSlide> getMapSlidesForSlideshow(int slideshowId) throws DAOException {
+    @Override public List<MapSlide> getMapSlidesForSlideshow(int slideshowId) throws DAOException {
         try {
-            return jdbcTemplate.query(MAP_READ_ALL_BY_SLIDESHOW_STATEMENT, new MapSlideMapper(), slideshowId);
+            return jdbcTemplate
+                    .query(MAP_READ_ALL_BY_SLIDESHOW_STATEMENT, new MapSlideMapper(), slideshowId);
         } catch (DataAccessException | DAOException.Unchecked ex) {
             throw new DAOException("Failed to retrieve map slides for given slideshow", ex);
         }
     }
 
-    @Override
-    public List<TitleSlide> getTitleSlidesForSlideshow(int slideshowId) throws DAOException {
+    @Override public List<TitleSlide> getTitleSlidesForSlideshow(int slideshowId)
+            throws DAOException {
         try {
-            return jdbcTemplate.query(TITLE_READ_ALL_BY_SLIDESHOW_STATEMENT, new TitleSlideMapper(), slideshowId);
+            return jdbcTemplate.query(TITLE_READ_ALL_BY_SLIDESHOW_STATEMENT, new TitleSlideMapper(),
+                    slideshowId);
         } catch (DataAccessException | DAOException.Unchecked ex) {
             throw new DAOException("Failed to retrieve title slides for given slideshow", ex);
         }
@@ -216,12 +195,8 @@ public class JDBCSlideDAO extends JDBCDAOBase implements SlideDAO {
         SlideValidator.validateID(slide.getId());
 
         try {
-            jdbcTemplate.update(BASE_UPDATE_STATEMENT,
-                    slide.getSlideshowId(),
-                    slide.getOrder(),
-                    slide.getCaption(),
-                    slide.getId()
-            );
+            jdbcTemplate.update(BASE_UPDATE_STATEMENT, slide.getSlideshowId(), slide.getOrder(),
+                    slide.getCaption(), slide.getId());
             logger.debug("Successfully updated slide");
             return slide;
         } catch (DataAccessException ex) {
@@ -231,8 +206,7 @@ public class JDBCSlideDAO extends JDBCDAOBase implements SlideDAO {
     }
 
     private class PhotoSlideMapper implements RowMapper<PhotoSlide> {
-        @Override
-        public PhotoSlide mapRow(ResultSet rs, int rowNum) throws SQLException {
+        @Override public PhotoSlide mapRow(ResultSet rs, int rowNum) throws SQLException {
             Photo photo;
 
             try {
@@ -246,16 +220,16 @@ public class JDBCSlideDAO extends JDBCDAOBase implements SlideDAO {
     }
 
     private class MapSlideMapper implements RowMapper<MapSlide> {
-        @Override
-        public MapSlide mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return new MapSlide(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4), rs.getDouble(5), rs.getDouble(6));
+        @Override public MapSlide mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new MapSlide(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4),
+                    rs.getDouble(5), rs.getDouble(6), rs.getInt(7));
         }
     }
 
     private class TitleSlideMapper implements RowMapper<TitleSlide> {
-        @Override
-        public TitleSlide mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return new TitleSlide(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4), rs.getInt(5));
+        @Override public TitleSlide mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new TitleSlide(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4),
+                    rs.getInt(5));
         }
     }
 }
