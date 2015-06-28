d6bfdf531b9942b784b90f3fe63c511dd50fc036
Enri Miho <enrimiho@gmail.com>
2015-06-28 02:40:43 +0200

    Move code for FlickrImageTile to control package

---
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/control/FlickrImageTile.java
@@ -0,0 +1,103 @@
+package at.ac.tuwien.qse.sepm.gui.control;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Widget for one widget in the image grid. Can either be in a selected or an unselected state.
+ */
+public class FlickrImageTile extends StackPane {
+
+    private static final Logger logger = LogManager.getLogger();
+    private static final String tmpDir = System.getProperty("java.io.tmpdir");
+    private final BorderPane overlay = new BorderPane();
+    private BooleanProperty selected = new SimpleBooleanProperty(false);
+    private com.flickr4java.flickr.photos.Photo flickrPhoto;
+
+    public FlickrImageTile(com.flickr4java.flickr.photos.Photo flickrPhoto) {
+        super();
+        this.flickrPhoto = flickrPhoto;
+        Image image = null;
+        try {
+            FileInputStream fis = new FileInputStream(new File(
+                    tmpDir+ flickrPhoto.getId()+"."+ flickrPhoto
+                    .getOriginalFormat()));
+            image = new Image(fis, 150, 0, true, true);
+            fis.close();
+        } catch (FileNotFoundException ex) {
+            logger.error("Could not find photo", ex);
+            return;
+        } catch (IOException e) {
+            logger.error("Could not close fileinputstream", e);
+        }
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(150);
+        imageView.setFitHeight(150);
+        getChildren().add(imageView);
+
+        setAlignment(overlay, Pos.BOTTOM_CENTER);
+        FontAwesomeIconView checkIcon = new FontAwesomeIconView();
+        checkIcon.setGlyphName("CHECK");
+        checkIcon.setStyle("-fx-fill: white");
+        overlay.setAlignment(checkIcon, Pos.CENTER_RIGHT);
+        overlay.setStyle("-fx-background-color: -tmg-secondary; -fx-max-height: 20px;");
+        overlay.setBottom(checkIcon);
+        getChildren().add(overlay);
+        overlay.setVisible(false);
+        setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent event) {
+                if(!getSelectedProperty().getValue()){
+                    select();
+                }
+                else{
+                    deselect();
+                }
+            }
+        });
+    }
+
+    public com.flickr4java.flickr.photos.Photo getFlickrPhoto() {
+        return flickrPhoto;
+    }
+
+    public void select() {
+        selected.setValue(true);
+        overlay.setVisible(true);
+        setStyle("-fx-border-color: -tmg-secondary");
+    }
+
+    public void deselect() {
+        selected.setValue(false);
+        overlay.setVisible(false);
+        setStyle("-fx-border-color: none");
+    }
+
+    /**
+     * Property which represents if this tile is currently selected or not.
+     *
+     * @return The selected property.
+     */
+    public BooleanProperty getSelectedProperty() {
+        return selected;
+    }
+
+    public boolean isSelected() {
+        return selected.getValue();
+    }
+
+}
--- src/main/java/at/ac/tuwien/qse/sepm/gui/dialogs/FlickrDialog.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/dialogs/FlickrDialog.java
@@ -5,6 +5,7 @@ import at.ac.tuwien.qse.sepm.entities.Photo;
 import at.ac.tuwien.qse.sepm.gui.FXMLLoadHelper;
 import at.ac.tuwien.qse.sepm.gui.FullscreenWindow;
 import at.ac.tuwien.qse.sepm.gui.control.DownloadProgressControl;
+import at.ac.tuwien.qse.sepm.gui.control.FlickrImageTile;
 import at.ac.tuwien.qse.sepm.gui.control.GoogleMapScene;
 import at.ac.tuwien.qse.sepm.gui.control.Keyword;
 import at.ac.tuwien.qse.sepm.gui.controller.Menu;
@@ -18,34 +19,25 @@ import at.ac.tuwien.qse.sepm.util.Cancelable;
 import at.ac.tuwien.qse.sepm.util.ErrorHandler;
 import at.ac.tuwien.qse.sepm.util.IOHandler;
 import com.flickr4java.flickr.tags.Tag;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.application.Platform;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -84,7 +76,6 @@ public class FlickrDialog extends ResultDialog<List<com.flickr4java.flickr.photo
     private ClusterService clusterService;
     private IOHandler ioHandler;
     private LatLong actualLatLong;
-    private FlickrImageTile lastSelected;
     private ArrayList<Photo> photos = new ArrayList<>();
     private Cancelable searchTask;
     private Cancelable downloadTask;
@@ -137,15 +128,18 @@ public class FlickrDialog extends ResultDialog<List<com.flickr4java.flickr.photo
         });
         scrollPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override public void handle(MouseEvent event) {
-                if (!event.isControlDown()) {
-                    for (Node n : photosFlowPane.getChildren()) {
-                        if(n instanceof FlickrImageTile){
-                            if(n==lastSelected) {
-                                ((FlickrImageTile) n).select();
-                            }
-                            else {
-                                ((FlickrImageTile) n).deselect();
-                            }
+                if(!event.isControlDown()){
+                    FlickrImageTile selected = null;
+                    Object target = event.getTarget();
+                    if(target instanceof ImageView){
+                        selected = (FlickrImageTile) ((ImageView) target).getParent();
+                    }
+                    for(Node n : photosFlowPane.getChildren()){
+                        if(n!=selected){
+                            ((FlickrImageTile) n).deselect();
+                        }
+                        else{
+                            ((FlickrImageTile) n).select();
                         }
                     }
                 }
@@ -379,88 +373,6 @@ public class FlickrDialog extends ResultDialog<List<com.flickr4java.flickr.photo
         searchButton.setDisable(false);
         progressIndicator.setVisible(false);
         importButton.setDisable(false);
-    }
-
-    /**
-     * Widget for one widget in the image grid. Can either be in a selected or an unselected state.
-     */
-    private class FlickrImageTile extends StackPane {
-
-        private final BorderPane overlay = new BorderPane();
-        private BooleanProperty selected = new SimpleBooleanProperty(false);
-        private com.flickr4java.flickr.photos.Photo flickrPhoto;
-
-        public FlickrImageTile(com.flickr4java.flickr.photos.Photo flickrPhoto) {
-            super();
-            this.flickrPhoto = flickrPhoto;
-            Image image = null;
-            try {
-                FileInputStream fis = new FileInputStream(new File(tmpDir+ flickrPhoto.getId()+"."+ flickrPhoto
-                        .getOriginalFormat()));
-                image = new Image(fis, 150, 0, true, true);
-                fis.close();
-            } catch (FileNotFoundException ex) {
-                logger.error("Could not find photo", ex);
-                return;
-            } catch (IOException e) {
-                logger.error("Could not close fileinputstream", e);
-            }
-            ImageView imageView = new ImageView(image);
-            imageView.setFitWidth(150);
-            imageView.setFitHeight(150);
-            getChildren().add(imageView);
-
-            setAlignment(overlay,Pos.BOTTOM_CENTER);
-            FontAwesomeIconView checkIcon = new FontAwesomeIconView();
-            checkIcon.setGlyphName("CHECK");
-            checkIcon.setStyle("-fx-fill: white");
-            overlay.setAlignment(checkIcon, Pos.CENTER_RIGHT);
-            overlay.setStyle("-fx-background-color: -tmg-secondary; -fx-max-height: 20px;");
-            overlay.setBottom(checkIcon);
-            getChildren().add(overlay);
-            overlay.setVisible(false);
-            setOnMouseClicked(new EventHandler<MouseEvent>() {
-                @Override public void handle(MouseEvent event) {
-                    if(!getSelectedProperty().getValue()){
-                        select();
-                    }
-                    else{
-                        deselect();
-                    }
-                }
-            });
-        }
-
-        public com.flickr4java.flickr.photos.Photo getFlickrPhoto() {
-            return flickrPhoto;
-        }
-
-        public void select() {
-            selected.setValue(true);
-            overlay.setVisible(true);
-            setStyle("-fx-border-color: -tmg-secondary");
-            lastSelected = this;
-        }
-
-        public void deselect() {
-            selected.setValue(false);
-            overlay.setVisible(false);
-            setStyle("-fx-border-color: none");
-        }
-
-        /**
-         * Property which represents if this tile is currently selected or not.
-         *
-         * @return The selected property.
-         */
-        public BooleanProperty getSelectedProperty() {
-            return selected;
-        }
-
-        public boolean isSelected() {
-            return selected.getValue();
-        }
-
     }
 
 }
