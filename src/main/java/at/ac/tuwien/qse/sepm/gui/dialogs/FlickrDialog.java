1ecdd59c9205dc618f0a5ee128f10c48ba939a60
Enri Miho <enrimiho@gmail.com>
2015-06-28 13:45:09 +0200

    Create combobox component for the flickr dialog

--- src/main/java/at/ac/tuwien/qse/sepm/gui/dialogs/FlickrDialog.java
+++ src/main/java/at/ac/tuwien/qse/sepm/gui/dialogs/FlickrDialog.java
@@ -30,15 +30,11 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
-import javafx.util.Callback;
-import javafx.util.StringConverter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -63,7 +59,7 @@ public class FlickrDialog extends ResultDialog<List<com.flickr4java.flickr.photo
     @FXML
     private FlickrDatePicker flickrDatePicker;
     @FXML
-    private ComboBox journeysComboBox;
+    private FlickrJourneysComboBox journeysComboBox;
     @FXML
     private GoogleMapScene mapScene;
 
@@ -143,48 +139,8 @@ public class FlickrDialog extends ResultDialog<List<com.flickr4java.flickr.photo
                 }
             }
         });
-        journeysComboBox.setCellFactory(new Callback<ListView<Journey>, ListCell<Journey>>() {
-            @Override public ListCell<Journey> call(ListView<Journey> l) {
-                return new ListCell<Journey>() {
-                    @Override protected void updateItem(Journey j, boolean empty) {
-                        super.updateItem(j, empty);
-                        if(j!=null){
-                            setText(j.getName());
-                        }
+        journeysComboBox.bindToFlickrDatePicker(flickrDatePicker);
 
-
-                    }
-                };
-            }
-        });
-        journeysComboBox.setConverter(new StringConverter<Journey>() {
-            @Override public String toString(Journey journey) {
-                if (journey == null) {
-                    return null;
-                } else {
-                    return journey.getName();
-                }
-            }
-
-            @Override public Journey fromString(String string) {
-                return null;
-            }
-
-        });
-        journeysComboBox.valueProperty().addListener(new ChangeListener<Journey>() {
-            @Override public void changed(ObservableValue ov, Journey oldValue, Journey newValue) {
-                if(newValue!=null && newValue.getId()!=-1){
-                    flickrDatePicker.setRanges(newValue.getStartDate(), newValue.getEndDate());
-                    flickrDatePicker.setValue(newValue.getStartDate().toLocalDate());
-                    logger.debug("Date set to {}", newValue.getStartDate().toLocalDate());
-                }
-                else if(newValue!=null && newValue.getId()==-1){
-                    flickrDatePicker.setRanges(LocalDateTime.MIN,LocalDateTime.MAX);
-                    flickrDatePicker.setValue(LocalDate.now());
-                    logger.debug("Today's date set");
-                }
-            }
-        });
         try {
             Journey j = new Journey(-1,"Keine Reise",null,null);
             List<Journey> journeys = clusterService.getAllJourneys();
